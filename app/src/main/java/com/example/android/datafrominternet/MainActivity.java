fd7ac1650553dcfabea33e050ea47e5da07b45ac
mlustig <mlustig@google.com>
2016-06-07 12:23:00 -0700

    T02.01-Exercise-CreateLayout

---
+++ app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.datafrominternet;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    // TODO (26) Create an EditText variable called mSearchBoxEditText
+
+    // TODO (27) Create a TextView variable called mUrlDisplayTextView
+    // TODO (28) Create a TextView variable called mSearchResultsTextView
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // TODO (29) Use findViewById to get a reference to mSearchBoxEditText
+
+        // TODO (30) Use findViewById to get a reference to mUrlDisplayTextView
+        // TODO (31) Use findViewById to get a reference to mSearchResultsTextView
+    }
+}
