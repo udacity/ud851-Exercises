709c4d694749ed3092692b1306180a3a9c3ad4b9
mlustig <mlustig@google.com>
2016-06-23 05:41:45 -0700

    T02.03-Exercise-DisplayUrl

--- app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -43,27 +43,24 @@ public class MainActivity extends AppCompatActivity {
         mSearchResultsTextView = (TextView) findViewById(R.id.tv_github_search_results_json);
     }
 
-    // COMPLETED (8) Override onCreateOptionsMenu
+    // TODO (2) Create a method called makeGithubSearchQuery
+    // TODO (3) Within this method, build the URL with the text from the EditText and set the built URL to the TextView
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // COMPLETED (9) Within onCreateOptionsMenu, use getMenuInflater().inflate to inflate the menu
         getMenuInflater().inflate(R.menu.main, menu);
-        // COMPLETED (10) Return true to display your menu
         return true;
     }
 
-    // COMPLETED (11) Override onOptionsItemSelected
-    // COMPLETED (12) Within onOptionsItemSelected, get the ID of the item that was selected
-    // COMPLETED (13) If the item's ID is R.id.action_search, show a Toast and return true to tell Android that you've handled this menu click
-    // COMPLETED (14) Don't forgot to call .show() on your Toast
-    // COMPLETED (15) If you do NOT handle the menu click, return super.onOptionsItemSelected to let Android handle the menu click
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int itemThatWasClickedId = item.getItemId();
         if (itemThatWasClickedId == R.id.action_search) {
+            // TODO (4) Remove the Toast message when the search menu item is clicked
             Context context = MainActivity.this;
             String textToShow = "Search clicked";
             Toast.makeText(context, textToShow, Toast.LENGTH_SHORT).show();
+            // TODO (5) Call makeGithubSearchQuery when the search menu item is clicked
             return true;
         }
         return super.onOptionsItemSelected(item);
