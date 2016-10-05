1123465d01abbc800a3cf763b558a695d1d44b86
Asser Samak <asser@asamak.local>
2016-10-04 18:02:34 -0700

    T07.03-Solution-GetAllTheData

--- app/src/main/java/com/example/android/waitlist/GuestListAdapter.java
+++ app/src/main/java/com/example/android/waitlist/GuestListAdapter.java
@@ -11,17 +11,19 @@ import android.widget.TextView;
 public class GuestListAdapter extends RecyclerView.Adapter<GuestListAdapter.GuestViewHolder> {
 
     private Context mContext;
-    // TODO (8) Add a new local variable mCount to store the count of items to be displayed in the recycler view
+    // COMPLETED (8) Add a new local variable mCount to store the count of items to be displayed in the recycler view
+    private int mCount;
 
+    // COMPLETED (9) Update the Adapter constructor to accept an integer for the count along with the context
     /**
      * Constructor using the context and the db cursor
      *
      * @param context the calling context/activity
      */
-    // TODO (9) Update the Adapter constructor to accept an integer for the count along with the context
-    public GuestListAdapter(Context context) {
+    public GuestListAdapter(Context context, int count) {
         this.mContext = context;
-        // TODO (10) Set the local mCount to be equal to count
+        // COMPLETED (10) Set the local mCount to be equal to count
+        mCount = count;
     }
 
     @Override
@@ -38,10 +40,10 @@ public class GuestListAdapter extends RecyclerView.Adapter<GuestListAdapter.Gues
     }
 
 
-    // TODO (11) Modify the getItemCount to return the mCount value rather than 0
+    // COMPLETED (11) Modify the getItemCount to return the mCount value rather than 0
     @Override
     public int getItemCount() {
-        return 0;
+        return mCount;
     }
 
 
