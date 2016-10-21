fa880f4de78044ba5d5887310ba02ac8f7f366da
Lyla <lyla@udacity.com>
2016-10-20 21:42:32 -0700

    T06.08-Exercise-PreferenceSummary

--- app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
+++ app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
@@ -27,7 +27,7 @@ import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v7.preference.PreferenceScreen;
 import android.widget.Toast;
 
-
+// TODO (1) Implement OnSharedPreferenceChangeListener
 public class SettingsFragment extends PreferenceFragmentCompat {
 
     @Override
@@ -35,6 +35,23 @@ public class SettingsFragment extends PreferenceFragmentCompat {
 
         // Add visualizer preferences, defined in the XML file in res->xml->pref_visualizer
         addPreferencesFromResource(R.xml.pref_visualizer);
+
+        // TODO (3) Get the preference screen, get the number of preferences and iterate through
+        // all of the preferences if it is not a checkbox preference, call the setSummary method
+        // passing in a preference and the value of the preference
     }
+
+    // TODO (4) Override onSharedPreferenceChanged and, if it is not a checkbox preference,
+    // call setPreferenceSummary on the changed preference
+
+    // TODO (2) Create a setPreferenceSummary which takes a Preference and String value as parameters.
+    // This method should check if the preference is a ListPreference and, if so, find the label
+    // associated with the value. You can do this by using the findIndexOfValue and getEntries methods
+    // of Preference.
+
+    // TODO (5) Register and unregister the OnSharedPreferenceChange listener (this class) in
+    // onCreate and onDestroy respectively.
+
+
 
 }
