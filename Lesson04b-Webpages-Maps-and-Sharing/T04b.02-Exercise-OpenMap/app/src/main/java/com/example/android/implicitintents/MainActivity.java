b05be8f93f4484004f7e2443f5b63aa8297251b6
Jeremy Silver <jeremy@udacity.com>
2016-12-07 16:49:15 -0800

    Fix up command line builds
    Except for lesson 3

--- Lesson04b-Webpages-Maps-and-Sharing/T04b.02-Exercise-OpenMap/app/src/main/java/com/example/android/implicitintents/MainActivity.java
+++ Lesson04b-Webpages-Maps-and-Sharing/T04b.02-Exercise-OpenMap/app/src/main/java/com/example/android/implicitintents/MainActivity.java
@@ -111,6 +111,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
     // TODO (1) Create a method called showMap with a Uri as the single parameter
     // Do steps 2 - 4 within the showMap method
         // TODO (2) Create an Intent with action type, Intent.ACTION_VIEW
@@ -118,5 +119,6 @@ public class MainActivity extends AppCompatActivity {
         // TODO (3) Set the data of the Intent to the Uri passed into this method
 
         // TODO (4) Verify that this Intent can be launched and then call startActivity
-    }
+
+
 }
