06bec9b427505b4aa8a302e1cf292b6b923907a7
seiko149 <tavanojason@gmail.com>
2016-12-14 12:27:47 -0800

    testt
    testt

--- Lesson02-GitHub-Repo-Search/T02.01-Solution-CreateLayout/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ Lesson02-GitHub-Repo-Search/T02.01-Solution-CreateLayout/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
     //test
     //test
+    //test
     // COMPLETED (26) Create an EditText variable called mSearchBoxEditText
     private EditText mSearchBoxEditText;
 
