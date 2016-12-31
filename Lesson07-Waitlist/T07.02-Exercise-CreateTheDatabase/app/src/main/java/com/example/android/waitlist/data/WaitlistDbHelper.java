5b4930fcf0c6c44b8a0582d522be8893e6a69ccb
Patrick Rice <patrickrice20@gmail.com>
2016-12-30 17:24:02 -0800

    wrong required type: Changed String -> int

--- Lesson07-Waitlist/T07.02-Exercise-CreateTheDatabase/app/src/main/java/com/example/android/waitlist/data/WaitlistDbHelper.java
+++ Lesson07-Waitlist/T07.02-Exercise-CreateTheDatabase/app/src/main/java/com/example/android/waitlist/data/WaitlistDbHelper.java
@@ -11,7 +11,7 @@ public class WaitlistDbHelper {
 
     // TODO (2) Create a static final String called DATABASE_NAME and set it to "waitlist.db"
 
-    // TODO (3) Create a static final String called DATABASE_VERSION and set it to 1
+    // TODO (3) Create a static final int called DATABASE_VERSION and set it to 1
 
     // TODO (4) Create a Constructor that takes a context and calls the parent constructor
 
