a664cb6b497b44f6e576b534252b1f76de3f7987
Jeremy Silver <jeremy@udacity.com>
2017-01-06 17:39:25 -0800

    T02.01-Solution-CreateLayout

--- app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -17,22 +17,30 @@ package com.example.android.datafrominternet;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO (26) Create an EditText variable called mSearchBoxEditText
+    // COMPLETED (26) Create an EditText variable called mSearchBoxEditText
+    private EditText mSearchBoxEditText;
 
-    // TODO (27) Create a TextView variable called mUrlDisplayTextView
-    // TODO (28) Create a TextView variable called mSearchResultsTextView
+    // COMPLETED (27) Create a TextView variable called mUrlDisplayTextView
+    private TextView mUrlDisplayTextView;
+    // COMPLETED (28) Create a TextView variable called mSearchResultsTextView
+    private TextView mSearchResultsTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // TODO (29) Use findViewById to get a reference to mSearchBoxEditText
+        // COMPLETED (29) Use findViewById to get a reference to mSearchBoxEditText
+        mSearchBoxEditText = (EditText) findViewById(R.id.et_search_box);
 
-        // TODO (30) Use findViewById to get a reference to mUrlDisplayTextView
-        // TODO (31) Use findViewById to get a reference to mSearchResultsTextView
+        // COMPLETED (30) Use findViewById to get a reference to mUrlDisplayTextView
+        mUrlDisplayTextView = (TextView) findViewById(R.id.tv_url_display);
+        // COMPLETED (31) Use findViewById to get a reference to mSearchResultsTextView
+        mSearchResultsTextView = (TextView) findViewById(R.id.tv_github_search_results_json);
     }
 }
