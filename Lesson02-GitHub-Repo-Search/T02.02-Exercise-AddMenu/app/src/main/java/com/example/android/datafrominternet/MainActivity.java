611d81709ff90b66c11f3e384668a01d6bc1ac4f
Davide Riva <driva95@protonmail.com>
2017-01-15 18:01:33 +0100

    "import android.content.Context;" added
    In the video lesson we have to use Content, but in the entire video no one import it. Should be imported by default

--- Lesson02-GitHub-Repo-Search/T02.02-Exercise-AddMenu/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ Lesson02-GitHub-Repo-Search/T02.02-Exercise-AddMenu/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -19,6 +19,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.content.Context;
 
 public class MainActivity extends AppCompatActivity {
 
