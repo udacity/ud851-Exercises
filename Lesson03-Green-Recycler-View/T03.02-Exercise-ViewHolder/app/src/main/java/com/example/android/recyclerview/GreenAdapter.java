87bf7d4ec0336362f5f87abec80777949ff59243
egmp777 <egmp777@gmail.com>
2017-02-27 17:42:52 -0500

    Completed Exercise T03.02
    Completed

--- Lesson03-Green-Recycler-View/T03.02-Exercise-ViewHolder/app/src/main/java/com/example/android/recyclerview/GreenAdapter.java
+++ Lesson03-Green-Recycler-View/T03.02-Exercise-ViewHolder/app/src/main/java/com/example/android/recyclerview/GreenAdapter.java
@@ -66,7 +66,6 @@ public class GreenAdapter extends RecyclerView.Adapter<GreenAdapter.NumberViewHo
     }
 
     /**
-     *
      * This gets called when each new ViewHolder is created. This happens when the RecyclerView
      * is laid out. Enough ViewHolders will be created to fill the screen and allow for scrolling.
      *
@@ -118,9 +117,20 @@ public class GreenAdapter extends RecyclerView.Adapter<GreenAdapter.NumberViewHo
     }
 
     // TODO (12) Create a class called NumberViewHolder that extends RecyclerView.ViewHolder
+    public class NumberViewHolder extends RecyclerView.ViewHolder{
 
+        TextView listItemNumberView;
+
+        public NumberViewHolder (View itemView){
+            super(itemView);
+            listItemNumberView = (TextView) itemView.findViewById(R.id.tv_item_number);
+        }
+
+        public void bind(int  listIndex){
+            listItemNumberView.setText(String.valueOf(listIndex));
+
+        }
     // TODO (13) Within NumberViewHolder, create a TextView variable called listItemNumberView
-
     // TODO (14) Create a constructor for NumberViewHolder that accepts a View called itemView as a parameter
     // TODO (15) Within the constructor, call super(itemView) and then find listItemNumberView by ID
 
@@ -128,5 +138,6 @@ public class GreenAdapter extends RecyclerView.Adapter<GreenAdapter.NumberViewHo
     // TODO (17) Within bind, set the text of listItemNumberView to the listIndex
     // TODO (18) Be careful to get the String representation of listIndex, as using setText with an int does something different
 
-    }
+}
+
 }
