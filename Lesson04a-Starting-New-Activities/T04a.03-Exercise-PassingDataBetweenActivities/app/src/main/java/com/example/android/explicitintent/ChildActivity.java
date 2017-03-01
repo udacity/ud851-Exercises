136dc6faa126c812bf51ca58af892ee89a3b36af
egmp777 <egmp777@gmail.com>
2017-03-01 17:31:16 -0500

    T04a.03-Exercise-PassingDataBetweenActivities
    Does

--- Lesson04a-Starting-New-Activities/T04a.03-Exercise-PassingDataBetweenActivities/app/src/main/java/com/example/android/explicitintent/ChildActivity.java
+++ Lesson04a-Starting-New-Activities/T04a.03-Exercise-PassingDataBetweenActivities/app/src/main/java/com/example/android/explicitintent/ChildActivity.java
@@ -15,6 +15,7 @@
  */
 package com.example.android.explicitintent;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
@@ -33,9 +34,12 @@ public class ChildActivity extends AppCompatActivity {
         mDisplayText = (TextView) findViewById(R.id.tv_display);
 
         // TODO (3) Use the getIntent method to store the Intent that started this Activity in a variable
-
+        Intent intentThatStartedThisActivity = getIntent();
         // TODO (4) Create an if statement to check if this Intent has the extra we passed from MainActivity
-
+        if(intentThatStartedThisActivity.hasExtra(Intent.EXTRA_TEXT)){
+            String textEntered = intentThatStartedThisActivity.getStringExtra(Intent.EXTRA_TEXT);
+            mDisplayText.setText(textEntered);
+        }
             // TODO (5) If the Intent contains the correct extra, retrieve the text
 
             // TODO (6) If the Intent contains the correct extra, use it to set the TextView text
