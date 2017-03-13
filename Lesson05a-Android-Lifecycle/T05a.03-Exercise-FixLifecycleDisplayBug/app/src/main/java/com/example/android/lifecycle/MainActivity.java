95b757712740a2aa3021e9407869da7809f2e193
egmp777 <egmp777@gmail.com>
2017-03-13 13:23:15 -0500

    T05a.03-Exercise-FixLifecycleDisplayBug
    Completed. The onDestroy and onStop events now appear in the textview
    when I rotate the device. The onRestart event appears when I relaunch
    the app

--- Lesson05a-Android-Lifecycle/T05a.03-Exercise-FixLifecycleDisplayBug/app/src/main/java/com/example/android/lifecycle/MainActivity.java
+++ Lesson05a-Android-Lifecycle/T05a.03-Exercise-FixLifecycleDisplayBug/app/src/main/java/com/example/android/lifecycle/MainActivity.java
@@ -6,6 +6,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.ListIterator;
+
 public class MainActivity extends AppCompatActivity {
 
     /*
@@ -40,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView mLifecycleDisplay;
 
     // TODO (1) Declare and instantiate a static ArrayList of Strings called mLifecycleCallbacks
+    private static ArrayList<String> mLifecycleCallbacks = new ArrayList<String>();
 
     /**
      * Called when the activity is first created. This is where you should do all of your normal
@@ -72,8 +76,13 @@ public class MainActivity extends AppCompatActivity {
         }
 
         // TODO (4) Iterate backwards through mLifecycleCallbacks, appending each String and a newline to mLifecycleDisplay
+        ListIterator iterator = mLifecycleCallbacks.listIterator(mLifecycleCallbacks.size());
+        while(iterator.hasPrevious()){
 
+            mLifecycleDisplay.append(iterator.previous()+"\n");
+        }
         // TODO (5) Clear mLifecycleCallbacks after iterating through it
+        mLifecycleCallbacks.clear();
 
         logAndAppend(ON_CREATE);
     }
@@ -134,7 +143,7 @@ public class MainActivity extends AppCompatActivity {
         super.onStop();
 
         // TODO (2) Add the ON_STOP String to the front of mLifecycleCallbacks
-
+        mLifecycleCallbacks.add(0,ON_STOP);
         logAndAppend(ON_STOP);
     }
 
@@ -161,7 +170,7 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
 
         // TODO (3) Add the ON_DESTROY String to the front of mLifecycleCallbacks
-
+        mLifecycleCallbacks.add(0,ON_DESTROY);
         logAndAppend(ON_DESTROY);
     }
 
