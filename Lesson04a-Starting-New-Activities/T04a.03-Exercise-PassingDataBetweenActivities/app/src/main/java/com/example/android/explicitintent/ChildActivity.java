0f8c8f74a046267d501409afd5a6d6948fc627e2
Edwin Ramirez Ventura <edwin9870@gmail.com>
2017-03-14 19:01:21 -0400

    Solution for T04a.03

--- Lesson04a-Starting-New-Activities/T04a.03-Exercise-PassingDataBetweenActivities/app/src/main/java/com/example/android/explicitintent/ChildActivity.java
+++ Lesson04a-Starting-New-Activities/T04a.03-Exercise-PassingDataBetweenActivities/app/src/main/java/com/example/android/explicitintent/ChildActivity.java
@@ -15,6 +15,7 @@
  */
 package com.example.android.explicitintent;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
@@ -32,12 +33,14 @@ public class ChildActivity extends AppCompatActivity {
         /* Typical usage of findViewById... */
         mDisplayText = (TextView) findViewById(R.id.tv_display);
 
-        // TODO (3) Use the getIntent method to store the Intent that started this Activity in a variable
-
-        // TODO (4) Create an if statement to check if this Intent has the extra we passed from MainActivity
-
-            // TODO (5) If the Intent contains the correct extra, retrieve the text
-
-            // TODO (6) If the Intent contains the correct extra, use it to set the TextView text
+        // TODO COMPLETED (3) Use the getIntent method to store the Intent that started this Activity in a variable
+        Intent parentIntent = getIntent();
+        // TODO COMPLETED (4) Create an if statement to check if this Intent has the extra we passed from MainActivity
+        if(parentIntent.getStringExtra(Intent.EXTRA_TEXT) != null ) {
+            // TODO COMPLETED (5) If the Intent contains the correct extra, retrieve the text
+            String msj = parentIntent.getStringExtra(Intent.EXTRA_TEXT);
+            // TODO COMPLETED (6) If the Intent contains the correct extra, use it to set the TextView text
+            mDisplayText.setText(msj);
+        }
     }
 }
