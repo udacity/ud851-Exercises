09e61ac216f10237b4213d5577cc5f356e87791a
egmp777 <egmp777@gmail.com>
2017-03-20 12:47:50 -0500

    Exercise T06.08
    Completed

--- Lesson06-Visualizer-Preferences/T06.08-Exercise-PreferenceSummary/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
+++ Lesson06-Visualizer-Preferences/T06.08-Exercise-PreferenceSummary/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
@@ -27,8 +27,10 @@ import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v7.preference.PreferenceScreen;
 import android.widget.Toast;
 
+import java.util.List;
+
 // TODO (1) Implement OnSharedPreferenceChangeListener
-public class SettingsFragment extends PreferenceFragmentCompat {
+public class SettingsFragment extends PreferenceFragmentCompat implements OnSharedPreferenceChangeListener{
 
     @Override
     public void onCreatePreferences(Bundle bundle, String s) {
@@ -37,12 +39,48 @@ public class SettingsFragment extends PreferenceFragmentCompat {
         addPreferencesFromResource(R.xml.pref_visualizer);
 
         // TODO (3) Get the preference screen, get the number of preferences and iterate through
+        SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
+        PreferenceScreen preferenceScreen = getPreferenceScreen();
         // all of the preferences if it is not a checkbox preference, call the setSummary method
         // passing in a preference and the value of the preference
+        int count = preferenceScreen.getPreferenceCount();
+        for(int i = 0; i < count; i++){
+            Preference preference = preferenceScreen.getPreference(i);
+            if( !(preference instanceof CheckBoxPreference)){
+                String value = sharedPreferences.getString(preference.getKey(),"");
+                //Call helper method
+                setPreferenceSummary(preference, value);
+            }
+        }
     }
 
+    private void setPreferenceSummary(Preference preference, String value){
+        if(preference instanceof ListPreference){
+            ListPreference listPreference = (ListPreference)preference;
+            //find the index of the list value changed by user
+            int index = listPreference.findIndexOfValue(value);
+            if(index >= 0){
+                //Gets the value in the list in the position index inside the entries collection
+                listPreference.setSummary(listPreference.getEntries()[index]);
+            }
+        }
+    }
     // TODO (4) Override onSharedPreferenceChanged and, if it is not a checkbox preference,
     // call setPreferenceSummary on the changed preference
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Preference preference = findPreference(key);
+        if(null != preference){
+           if(!(preference instanceof  CheckBoxPreference)){
+               //value that the user changed
+               String value = sharedPreferences.getString(preference.getKey(), "");
+               //Call the utility method to set the preference in the list
+               setPreferenceSummary(preference, value);
+           }
+        }
+    }
+
+
 
     // TODO (2) Create a setPreferenceSummary which takes a Preference and String value as parameters.
     // This method should check if the preference is a ListPreference and, if so, find the label
@@ -51,6 +89,20 @@ public class SettingsFragment extends PreferenceFragmentCompat {
 
     // TODO (5) Register and unregister the OnSharedPreferenceChange listener (this class) in
     // onCreate and onDestroy respectively.
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        getPreferenceScreen().getSharedPreferences().
+                registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        getPreferenceScreen().getSharedPreferences().
+                unregisterOnSharedPreferenceChangeListener(this);
+
+    }
 
 
 
