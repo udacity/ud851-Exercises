602c06313574b39e3c8eac0adb5ed58f2ab4f298
egmp777 <egmp777@gmail.com>
2017-03-23 15:32:28 -0500

    Exercise T07.05
    Part 1 Completed

--- Lesson07-Waitlist/T07.05-Exercise-AddGuests/app/src/main/java/com/example/android/waitlist/MainActivity.java
+++ Lesson07-Waitlist/T07.05-Exercise-AddGuests/app/src/main/java/com/example/android/waitlist/MainActivity.java
@@ -21,7 +21,9 @@ public class MainActivity extends AppCompatActivity {
     private GuestListAdapter mAdapter;
     private SQLiteDatabase mDb;
 
-    // TODO (1) Create local EditText members for mNewGuestNameEditText and mNewPartySizeEditText
+    // COMPL TODO (1) Create local EditText members for mNewGuestNameEditText and mNewPartySizeEditText
+    private EditText mNewGuestNameEditText;
+    private EditText mNewPartySizeEditText;
 
     // TODO (13) Create a constant string LOG_TAG that is equal to the class.getSimpleName()
 
@@ -36,6 +38,9 @@ public class MainActivity extends AppCompatActivity {
         waitlistRecyclerView = (RecyclerView) this.findViewById(R.id.all_guests_list_view);
 
         // TODO (2) Set the Edit texts to the corresponding views using findViewById
+       mNewGuestNameEditText = (EditText)findViewById(R.id.person_name_edit_text);
+       mNewPartySizeEditText = (EditText) findViewById(R.id.party_count_edit_text);
+
 
         // Set layout for the RecyclerView, because it's a list we are using the linear layout
         waitlistRecyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -48,8 +53,8 @@ public class MainActivity extends AppCompatActivity {
         // because you will be adding restaurant customers
         mDb = dbHelper.getWritableDatabase();
 
-        // TODO (3) Remove this fake data call since we will be inserting our own data now
-        TestUtil.insertFakeData(mDb);
+        // COMPL TODO (3) Remove this fake data call since we will be inserting our own data now
+
 
         // Get all guest info from the database and save in a cursor
         Cursor cursor = getAllGuests();
@@ -69,16 +74,25 @@ public class MainActivity extends AppCompatActivity {
      */
     public void addToWaitlist(View view) {
 
-        // TODO (9) First thing, check if any of the EditTexts are empty, return if so
+        // COMPL TODO (9) First thing, check if any of the EditTexts are empty, return if so
+        if(mNewGuestNameEditText.toString().length()==0 || mNewPartySizeEditText.toString().length()==0){
+            return;
+        }
 
-        // TODO (10) Create an integer to store the party size and initialize to 1
+        // COMPL TODO (10) Create an integer to store the party size and initialize to 1
+        int partySize = 1;
 
-        // TODO (11) Use Integer.parseInt to parse mNewPartySizeEditText.getText to an integer
-
-        // TODO (12) Make sure you surround the Integer.parseInt with a try catch and log any exception
+        // COMPL TODO (11) Use Integer.parseInt to parse mNewPartySizeEditText.getText to an integer
+        try{
+            partySize = Integer.parseInt(mNewPartySizeEditText.getText().toString());
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        // COMPL TODO (12) Make sure you surround the Integer.parseInt with a try catch and log any exception
 
         // TODO (14) call addNewGuest with the guest name and party size
 
+        addGuest(mNewGuestNameEditText.getText().toString(), partySize);
         // TODO (19) call mAdapter.swapCursor to update the cursor by passing in getAllGuests()
 
         // TODO (20) To make the UI look nice, call .getText().clear() on both EditTexts, also call clearFocus() on mNewPartySizeEditText
@@ -105,10 +119,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // TODO (4) Create a new addGuest method
+    private long addGuest(String name, int partySize){
+        // COMPL TODO (5) Inside, create a ContentValues instance to pass the values onto the insert query
+        ContentValues cv = new ContentValues();
+        // TODO (6) call put to insert the name value with the key COLUMN_GUEST_NAME
+        cv.put(WaitlistContract.WaitlistEntry.COLUMN_GUEST_NAME, name);
+        cv.put(WaitlistContract.WaitlistEntry.COLUMN_PARTY_SIZE, partySize);
+        return mDb.insert(WaitlistContract.WaitlistEntry.TABLE_NAME,null,cv);
 
-    // TODO (5) Inside, create a ContentValues instance to pass the values onto the insert query
+    }
 
-    // TODO (6) call put to insert the name value with the key COLUMN_GUEST_NAME
+
+
 
     // TODO (7) call put to insert the party size value with the key COLUMN_PARTY_SIZE
 
