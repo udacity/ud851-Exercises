81a9ddd13c10cc5bb66d208c8d9eacd5584cb068
egmp777 <egmp777@gmail.com>
2017-03-23 17:06:28 -0500

    Exercise T07.05
    Completed

--- Lesson07-Waitlist/T07.05-Exercise-AddGuests/app/src/main/java/com/example/android/waitlist/GuestListAdapter.java
+++ Lesson07-Waitlist/T07.05-Exercise-AddGuests/app/src/main/java/com/example/android/waitlist/GuestListAdapter.java
@@ -57,13 +57,25 @@ public class GuestListAdapter extends RecyclerView.Adapter<GuestListAdapter.Gues
         return mCursor.getCount();
     }
 
-    // TODO (15) Create a new function called swapCursor that takes the new cursor and returns void
+    // COMPL TODO (15) Create a new function called swapCursor that takes the new cursor and returns void
+    public void swapCursor(Cursor newCursor){
+        // COMPL TODO (16) Inside, check if the current cursor is not null, and close it if so
+        if (mCursor == null){
+            mCursor.close();
+        }
+        // COMPL TODO (17) Update the local mCursor to be equal to  newCursor
+        mCursor = newCursor;
+        // COMPL TODO (18) Check if the newCursor is not null, and call this.notifyDataSetChanged() if so
+        if(newCursor != null){
+            this.notifyDataSetChanged();
+        }
+    }
 
-    // TODO (16) Inside, check if the current cursor is not null, and close it if so
 
-    // TODO (17) Update the local mCursor to be equal to  newCursor
 
-    // TODO (18) Check if the newCursor is not null, and call this.notifyDataSetChanged() if so
+
+
+
 
     /**
      * Inner class to hold the views needed to display a single item in the recycler-view
--- Lesson07-Waitlist/T07.05-Exercise-AddGuests/app/src/main/java/com/example/android/waitlist/MainActivity.java
+++ Lesson07-Waitlist/T07.05-Exercise-AddGuests/app/src/main/java/com/example/android/waitlist/MainActivity.java
@@ -15,6 +15,8 @@ import com.example.android.waitlist.data.TestUtil;
 import com.example.android.waitlist.data.WaitlistContract;
 import com.example.android.waitlist.data.WaitlistDbHelper;
 
+import static android.webkit.ConsoleMessage.MessageLevel.LOG;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,7 +27,8 @@ public class MainActivity extends AppCompatActivity {
     private EditText mNewGuestNameEditText;
     private EditText mNewPartySizeEditText;
 
-    // TODO (13) Create a constant string LOG_TAG that is equal to the class.getSimpleName()
+    // COMPL TODO (13) Create a constant string LOG_TAG that is equal to the class.getSimpleName()
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
         // Set local attributes to corresponding views
         waitlistRecyclerView = (RecyclerView) this.findViewById(R.id.all_guests_list_view);
 
-        // TODO (2) Set the Edit texts to the corresponding views using findViewById
+        // COMPL TODO (2) Set the Edit texts to the corresponding views using findViewById
        mNewGuestNameEditText = (EditText)findViewById(R.id.person_name_edit_text);
        mNewPartySizeEditText = (EditText) findViewById(R.id.party_count_edit_text);
 
@@ -86,16 +89,20 @@ public class MainActivity extends AppCompatActivity {
         try{
             partySize = Integer.parseInt(mNewPartySizeEditText.getText().toString());
         }catch(Exception e){
+            Log.e(LOG_TAG, e.getMessage());
             e.printStackTrace();
         }
         // COMPL TODO (12) Make sure you surround the Integer.parseInt with a try catch and log any exception
 
-        // TODO (14) call addNewGuest with the guest name and party size
+        // COMPL TODO (14) call addNewGuest with the guest name and party size
 
         addGuest(mNewGuestNameEditText.getText().toString(), partySize);
         // TODO (19) call mAdapter.swapCursor to update the cursor by passing in getAllGuests()
-
+        mAdapter.swapCursor(getAllGuests());
         // TODO (20) To make the UI look nice, call .getText().clear() on both EditTexts, also call clearFocus() on mNewPartySizeEditText
+        mNewGuestNameEditText.getText().clear();
+        mNewPartySizeEditText.getText().clear();
+        mNewPartySizeEditText.clearFocus();
 
     }
 
