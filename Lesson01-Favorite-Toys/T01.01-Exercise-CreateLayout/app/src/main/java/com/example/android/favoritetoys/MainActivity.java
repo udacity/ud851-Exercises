81640e2d5867d53883f2ddb8efe11c6dc6e9fa51
Ehbraheem <ehbraheem@gmail.com>
2017-03-31 23:24:38 -0700

    Add TODO(s)

--- Lesson01-Favorite-Toys/T01.01-Exercise-CreateLayout/app/src/main/java/com/example/android/favoritetoys/MainActivity.java
+++ Lesson01-Favorite-Toys/T01.01-Exercise-CreateLayout/app/src/main/java/com/example/android/favoritetoys/MainActivity.java
@@ -21,14 +21,19 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-//    private TextView mTextView;
+    // TODO (1) Declare a TextView variable called mToysListTextView
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        mTextView = findViewById(android.R.layout.textView);
+        // TODO (3) Use findViewById to get a reference to the TextView from the layout
+
+        // TODO (4) Use the static ToyBox.getToyNames method and store the names in a String array
+
+        // TODO (5) Loop through each toy and append the name to the TextView (add \n for spacing)
 
     }
 }
