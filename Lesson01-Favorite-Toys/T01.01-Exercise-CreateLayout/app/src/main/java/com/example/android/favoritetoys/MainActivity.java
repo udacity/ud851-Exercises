756cee02f49172b33d1d9e59613f4b059584962b
Ehbraheem <ehbraheem@gmail.com>
2017-03-31 23:08:38 -0700

    CreateLayout: Completed

--- Lesson01-Favorite-Toys/T01.01-Exercise-CreateLayout/app/src/main/java/com/example/android/favoritetoys/MainActivity.java
+++ Lesson01-Favorite-Toys/T01.01-Exercise-CreateLayout/app/src/main/java/com/example/android/favoritetoys/MainActivity.java
@@ -17,12 +17,18 @@ package com.example.android.favoritetoys;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+//    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        mTextView = findViewById(android.R.layout.textView);
+
     }
 }
