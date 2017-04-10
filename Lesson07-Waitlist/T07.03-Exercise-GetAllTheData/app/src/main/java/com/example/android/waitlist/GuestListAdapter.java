f9f8dc310ad3f9230a30f8ef6c8c1a73156fad96
Ehbraheem <ehbraheem@gmail.com>
2017-04-10 15:24:04 +0100

    SQLite: Get all the Data => Completed

--- Lesson07-Waitlist/T07.03-Exercise-GetAllTheData/app/src/main/java/com/example/android/waitlist/GuestListAdapter.java
+++ Lesson07-Waitlist/T07.03-Exercise-GetAllTheData/app/src/main/java/com/example/android/waitlist/GuestListAdapter.java
@@ -12,6 +12,7 @@ public class GuestListAdapter extends RecyclerView.Adapter<GuestListAdapter.Gues
 
     private Context mContext;
     // TODO (8) Add a new local variable mCount to store the count of items to be displayed in the recycler view
+    private int mCount;
 
     /**
      * Constructor using the context and the db cursor
@@ -19,9 +20,10 @@ public class GuestListAdapter extends RecyclerView.Adapter<GuestListAdapter.Gues
      * @param context the calling context/activity
      */
     // TODO (9) Update the Adapter constructor to accept an integer for the count along with the context
-    public GuestListAdapter(Context context) {
+    public GuestListAdapter(Context context, int mCount) {
         this.mContext = context;
         // TODO (10) Set the local mCount to be equal to count
+        this.mCount = mCount;
     }
 
     @Override
@@ -41,7 +43,7 @@ public class GuestListAdapter extends RecyclerView.Adapter<GuestListAdapter.Gues
     // TODO (11) Modify the getItemCount to return the mCount value rather than 0
     @Override
     public int getItemCount() {
-        return 0;
+        return mCount;
     }
 
 
--- Lesson07-Waitlist/T07.03-Exercise-GetAllTheData/app/src/main/java/com/example/android/waitlist/MainActivity.java
+++ Lesson07-Waitlist/T07.03-Exercise-GetAllTheData/app/src/main/java/com/example/android/waitlist/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.android.waitlist;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+
+import com.example.android.waitlist.data.TestUtil;
+import com.example.android.waitlist.data.WaitlistContract;
+import com.example.android.waitlist.data.WaitlistDbHelper;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private GuestListAdapter mAdapter;
 
     // TODO (1) Create a local field member of type SQLiteDatabase called mDb
+    private SQLiteDatabase mDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,17 +33,20 @@ public class MainActivity extends AppCompatActivity {
         // Set layout for the RecyclerView, because it's a list we are using the linear layout
         waitlistRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        // Create an adapter for that cursor to display the data
-        mAdapter = new GuestListAdapter(this);
-
         // TODO (2) Create a WaitlistDbHelper instance, pass "this" to the constructor as context
+        WaitlistDbHelper mDBHelper = new WaitlistDbHelper(this);
 
         // TODO (3) Get a writable database reference using getWritableDatabase and store it in mDb
+        mDb = mDBHelper.getWritableDatabase();
 
         // TODO (4) call insertFakeData from TestUtil and pass the database reference mDb
+        TestUtil.insertFakeData(mDb);
 
         // TODO (7) Run the getAllGuests function and store the result in a Cursor variable
+        Cursor cursor = getAllGuest();
 
+        // Create an adapter for that cursor to display the data
+        mAdapter = new GuestListAdapter(this, cursor.getCount());
         // TODO (12) Pass the resulting cursor count to the adapter
 
         // Link the adapter to the RecyclerView
@@ -54,7 +64,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // TODO (5) Create a private method called getAllGuests that returns a cursor
+    private Cursor getAllGuest() {
 
-    // TODO (6) Inside, call query on mDb passing in the table name and projection String [] order by COLUMN_TIMESTAMP
+        // TODO (6) Inside, call query on mDb passing in the table name and projection String [] order by COLUMN_TIMESTAMP
+        return mDb.query(
+                WaitlistContract.WaitlistEntry.TABLE_NAME,
+                null,
+                null,
+                null,
+                null,
+                null,
+                WaitlistContract.WaitlistEntry.COLUMN_TIMESTAMP
+        );
+    }
 
 }
