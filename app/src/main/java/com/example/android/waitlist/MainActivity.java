180593eff2bb2af86a7b92fd71528788938003a1
Asser <asser@udacity.com>
2017-04-12 21:51:17 +0200

    T07.05-Exercise-AddGuests

--- app/src/main/java/com/example/android/waitlist/MainActivity.java
+++ app/src/main/java/com/example/android/waitlist/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.android.waitlist;
 
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 import com.example.android.waitlist.data.TestUtil;
 import com.example.android.waitlist.data.WaitlistContract;
@@ -16,9 +19,11 @@ import com.example.android.waitlist.data.WaitlistDbHelper;
 public class MainActivity extends AppCompatActivity {
 
     private GuestListAdapter mAdapter;
-
     private SQLiteDatabase mDb;
 
+    // TODO (1) Create local EditText members for mNewGuestNameEditText and mNewPartySizeEditText
+
+    // TODO (13) Create a constant string LOG_TAG that is equal to the class.getSimpleName()
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
 
         // Set local attributes to corresponding views
         waitlistRecyclerView = (RecyclerView) this.findViewById(R.id.all_guests_list_view);
+
+        // TODO (2) Set the Edit texts to the corresponding views using findViewById
 
         // Set layout for the RecyclerView, because it's a list we are using the linear layout
         waitlistRecyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -41,13 +48,12 @@ public class MainActivity extends AppCompatActivity {
         // because you will be adding restaurant customers
         mDb = dbHelper.getWritableDatabase();
 
-        //Fill the database with fake data
+        // TODO (3) Remove this fake data call since we will be inserting our own data now
         TestUtil.insertFakeData(mDb);
 
         // Get all guest info from the database and save in a cursor
         Cursor cursor = getAllGuests();
 
-        // COMPLETED (10) Pass the entire cursor to the adapter rather than just the count
         // Create an adapter for that cursor to display the data
         mAdapter = new GuestListAdapter(this, cursor);
 
@@ -62,6 +68,20 @@ public class MainActivity extends AppCompatActivity {
      * @param view The calling view (button)
      */
     public void addToWaitlist(View view) {
+
+        // TODO (9) First thing, check if any of the EditTexts are empty, return if so
+
+        // TODO (10) Create an integer to store the party size and initialize to 1
+
+        // TODO (11) Use Integer.parseInt to parse mNewPartySizeEditText.getText to an integer
+
+        // TODO (12) Make sure you surround the Integer.parseInt with a try catch and log any exception
+
+        // TODO (14) call addNewGuest with the guest name and party size
+
+        // TODO (19) call mAdapter.swapCursor to update the cursor by passing in getAllGuests()
+
+        // TODO (20) To make the UI look nice, call .getText().clear() on both EditTexts, also call clearFocus() on mNewPartySizeEditText
 
     }
 
@@ -83,6 +103,17 @@ public class MainActivity extends AppCompatActivity {
                 WaitlistContract.WaitlistEntry.COLUMN_TIMESTAMP
         );
     }
+
+    // TODO (4) Create a new addGuest method
+
+    // TODO (5) Inside, create a ContentValues instance to pass the values onto the insert query
+
+    // TODO (6) call put to insert the name value with the key COLUMN_GUEST_NAME
+
+    // TODO (7) call put to insert the party size value with the key COLUMN_PARTY_SIZE
+
+    // TODO (8) call insert to run an insert query on TABLE_NAME with the ContentValues created
+
 
 
 }
