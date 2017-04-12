2de639ef2c265822e1602c8c2a802a4786e1dcf9
Asser <asser@udacity.com>
2017-04-12 22:17:37 +0200

    T09.03-Solution-UriMatcher

--- app/src/androidTest/java/com/example/android/todolist/data/TestTaskContentProvider.java
+++ app/src/androidTest/java/com/example/android/todolist/data/TestTaskContentProvider.java
@@ -18,9 +18,11 @@ package com.example.android.todolist.data;
 
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.UriMatcher;
 import android.content.pm.PackageManager;
 import android.content.pm.ProviderInfo;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -123,39 +125,39 @@ public class TestTaskContentProvider {
     //================================================================================
 
 
-//    private static final Uri TEST_TASKS = TaskContract.TaskEntry.CONTENT_URI;
-//    // Content URI for a single task with id = 1
-//    private static final Uri TEST_TASK_WITH_ID = TEST_TASKS.buildUpon().appendPath("1").build();
-//
-//
-//    /**
-//     * This function tests that the UriMatcher returns the correct integer value for
-//     * each of the Uri types that the ContentProvider can handle. Uncomment this when you are
-//     * ready to test your UriMatcher.
-//     */
-//    @Test
-//    public void testUriMatcher() {
-//
-//        /* Create a URI matcher that the TaskContentProvider uses */
-//        UriMatcher testMatcher = TaskContentProvider.buildUriMatcher();
-//
-//        /* Test that the code returned from our matcher matches the expected TASKS int */
-//        String tasksUriDoesNotMatch = "Error: The TASKS URI was matched incorrectly.";
-//        int actualTasksMatchCode = testMatcher.match(TEST_TASKS);
-//        int expectedTasksMatchCode = TaskContentProvider.TASKS;
-//        assertEquals(tasksUriDoesNotMatch,
-//                actualTasksMatchCode,
-//                expectedTasksMatchCode);
-//
-//        /* Test that the code returned from our matcher matches the expected TASK_WITH_ID */
-//        String taskWithIdDoesNotMatch =
-//                "Error: The TASK_WITH_ID URI was matched incorrectly.";
-//        int actualTaskWithIdCode = testMatcher.match(TEST_TASK_WITH_ID);
-//        int expectedTaskWithIdCode = TaskContentProvider.TASK_WITH_ID;
-//        assertEquals(taskWithIdDoesNotMatch,
-//                actualTaskWithIdCode,
-//                expectedTaskWithIdCode);
-//    }
+    private static final Uri TEST_TASKS = TaskContract.TaskEntry.CONTENT_URI;
+    // Content URI for a single task with id = 1
+    private static final Uri TEST_TASK_WITH_ID = TEST_TASKS.buildUpon().appendPath("1").build();
+
+
+    /**
+     * This function tests that the UriMatcher returns the correct integer value for
+     * each of the Uri types that the ContentProvider can handle. Uncomment this when you are
+     * ready to test your UriMatcher.
+     */
+    @Test
+    public void testUriMatcher() {
+
+        /* Create a URI matcher that the TaskContentProvider uses */
+        UriMatcher testMatcher = TaskContentProvider.buildUriMatcher();
+
+        /* Test that the code returned from our matcher matches the expected TASKS int */
+        String tasksUriDoesNotMatch = "Error: The TASKS URI was matched incorrectly.";
+        int actualTasksMatchCode = testMatcher.match(TEST_TASKS);
+        int expectedTasksMatchCode = TaskContentProvider.TASKS;
+        assertEquals(tasksUriDoesNotMatch,
+                actualTasksMatchCode,
+                expectedTasksMatchCode);
+
+        /* Test that the code returned from our matcher matches the expected TASK_WITH_ID */
+        String taskWithIdDoesNotMatch =
+                "Error: The TASK_WITH_ID URI was matched incorrectly.";
+        int actualTaskWithIdCode = testMatcher.match(TEST_TASK_WITH_ID);
+        int expectedTaskWithIdCode = TaskContentProvider.TASK_WITH_ID;
+        assertEquals(taskWithIdDoesNotMatch,
+                actualTaskWithIdCode,
+                expectedTaskWithIdCode);
+    }
 
 
     //================================================================================
