16985e7da098d1d593d8657ee1660bd899099756
Asser <asser@udacity.com>
2017-04-12 21:48:42 +0200

    T07.03-Solution-GetAllTheData

--- app/src/main/java/com/example/android/waitlist/MainActivity.java
+++ app/src/main/java/com/example/android/waitlist/MainActivity.java
@@ -1,17 +1,24 @@
 package com.example.android.waitlist;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
+import com.example.android.waitlist.data.TestUtil;
+import com.example.android.waitlist.data.WaitlistContract;
+import com.example.android.waitlist.data.WaitlistDbHelper;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private GuestListAdapter mAdapter;
 
-    // TODO (1) Create a local field member of type SQLiteDatabase called mDb
+    // COMPLETED (1) Create a local field SQLiteDatabase called mDb
+    private SQLiteDatabase mDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,18 +33,25 @@ public class MainActivity extends AppCompatActivity {
         // Set layout for the RecyclerView, because it's a list we are using the linear layout
         waitlistRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        // COMPLETED (2) Create a WaitlistDbHelper instance, pass "this" to the constructor
+        // Create a DB helper (this will create the DB if run for the first time)
+        WaitlistDbHelper dbHelper = new WaitlistDbHelper(this);
+
+        // COMPLETED (3) Get a writable database reference using getWritableDatabase and store it in mDb
+        // Keep a reference to the mDb until paused or killed. Get a writable database
+        // because you will be adding restaurant customers
+        mDb = dbHelper.getWritableDatabase();
+
+        // COMPLETED (4) call insertFakeData in TestUtil and pass the database reference mDb
+        //Fill the database with fake data
+        TestUtil.insertFakeData(mDb);
+
+        // COMPLETED (7) Run the getAllGuests function and store the result in a Cursor variable
+        Cursor cursor = getAllGuests();
+
+        // COMPLETED (12) Pass the resulting cursor count to the adapter
         // Create an adapter for that cursor to display the data
-        mAdapter = new GuestListAdapter(this);
-
-        // TODO (2) Create a WaitlistDbHelper instance, pass "this" to the constructor as context
-
-        // TODO (3) Get a writable database reference using getWritableDatabase and store it in mDb
-
-        // TODO (4) call insertFakeData from TestUtil and pass the database reference mDb
-
-        // TODO (7) Run the getAllGuests function and store the result in a Cursor variable
-
-        // TODO (12) Pass the resulting cursor count to the adapter
+        mAdapter = new GuestListAdapter(this, cursor.getCount());
 
         // Link the adapter to the RecyclerView
         waitlistRecyclerView.setAdapter(mAdapter);
@@ -53,8 +67,25 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    // TODO (5) Create a private method called getAllGuests that returns a cursor
 
-    // TODO (6) Inside, call query on mDb passing in the table name and projection String [] order by COLUMN_TIMESTAMP
+    // COMPLETED (5) Create a private method called getAllGuests that returns a cursor
+    /**
+     * Query the mDb and get all guests from the waitlist table
+     *
+     * @return Cursor containing the list of guests
+     */
+    private Cursor getAllGuests() {
+        // COMPLETED (6) Inside, call query on mDb passing in the table name and projection String [] order by COLUMN_TIMESTAMP
+        return mDb.query(
+                WaitlistContract.WaitlistEntry.TABLE_NAME,
+                null,
+                null,
+                null,
+                null,
+                null,
+                WaitlistContract.WaitlistEntry.COLUMN_TIMESTAMP
+        );
+    }
+
 
 }
