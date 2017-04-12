d3c0163c437fa283e8be0f56bd9c6f8259d04b28
Asser <asser@udacity.com>
2017-04-12 21:19:38 +0200

    T04a.03-Exercise-PassingDataBetweenActivities

--- app/src/main/java/com/example/android/explicitintent/ChildActivity.java
+++ app/src/main/java/com/example/android/explicitintent/ChildActivity.java
@@ -31,5 +31,13 @@ public class ChildActivity extends AppCompatActivity {
 
         /* Typical usage of findViewById... */
         mDisplayText = (TextView) findViewById(R.id.tv_display);
+
+        // TODO (3) Use the getIntent method to store the Intent that started this Activity in a variable
+
+        // TODO (4) Create an if statement to check if this Intent has the extra we passed from MainActivity
+
+            // TODO (5) If the Intent contains the correct extra, retrieve the text
+
+            // TODO (6) If the Intent contains the correct extra, use it to set the TextView text
     }
 }
