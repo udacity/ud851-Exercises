302941577474bc4a8191af53266d613e6bee25dc
Asser <asser@udacity.com>
2017-04-12 21:48:24 +0200

    T07.03-Exercise-GetAllTheData

--- app/src/main/java/com/example/android/waitlist/MainActivity.java
+++ app/src/main/java/com/example/android/waitlist/MainActivity.java
@@ -11,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
 
     private GuestListAdapter mAdapter;
 
+    // TODO (1) Create a local field member of type SQLiteDatabase called mDb
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,11 +29,20 @@ public class MainActivity extends AppCompatActivity {
         // Create an adapter for that cursor to display the data
         mAdapter = new GuestListAdapter(this);
 
+        // TODO (2) Create a WaitlistDbHelper instance, pass "this" to the constructor as context
+
+        // TODO (3) Get a writable database reference using getWritableDatabase and store it in mDb
+
+        // TODO (4) call insertFakeData from TestUtil and pass the database reference mDb
+
+        // TODO (7) Run the getAllGuests function and store the result in a Cursor variable
+
+        // TODO (12) Pass the resulting cursor count to the adapter
+
         // Link the adapter to the RecyclerView
         waitlistRecyclerView.setAdapter(mAdapter);
 
     }
-
 
     /**
      * This method is called when user clicks on the Add to waitlist button
@@ -42,5 +53,8 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    // TODO (5) Create a private method called getAllGuests that returns a cursor
+
+    // TODO (6) Inside, call query on mDb passing in the table name and projection String [] order by COLUMN_TIMESTAMP
 
 }
