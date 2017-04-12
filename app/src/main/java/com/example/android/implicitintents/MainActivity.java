66f065e1ecc01b476404e5d3efcf9263a2bd4ed6
Asser <asser@udacity.com>
2017-04-12 23:15:20 +0200

    T04b.02-Exercise-OpenMap

--- app/src/main/java/com/example/android/implicitintents/MainActivity.java
+++ app/src/main/java/com/example/android/implicitintents/MainActivity.java
@@ -37,10 +37,7 @@ public class MainActivity extends AppCompatActivity {
      * @param v Button that was clicked.
      */
     public void onClickOpenWebpageButton(View v) {
-        // COMPLETED (5) Create a String that contains a URL ( make sure it starts with http:// or https:// )
         String urlAsString = "http://www.udacity.com";
-
-        // COMPLETED (6) Replace the Toast with a call to openWebPage, passing in the URL String from the previous step
         openWebPage(urlAsString);
     }
 
@@ -51,6 +48,11 @@ public class MainActivity extends AppCompatActivity {
      * @param v Button that was clicked.
      */
     public void onClickOpenAddressButton(View v) {
+        // TODO (5) Store an address in a String
+
+        // TODO (6) Use Uri.Builder with the appropriate scheme and query to form the Uri for the address
+
+        // TODO (7) Replace the Toast with a call to showMap, passing in the Uri from the previous step
         Toast.makeText(this, "TODO: Open a map when this button is clicked", Toast.LENGTH_SHORT).show();
     }
 
@@ -80,7 +82,6 @@ public class MainActivity extends AppCompatActivity {
                 .show();
     }
 
-    // COMPLETED (1) Create a method called openWebPage that accepts a String as a parameter
     /**
      * This method fires off an implicit Intent to open a webpage.
      *
@@ -88,21 +89,18 @@ public class MainActivity extends AppCompatActivity {
      *            scheme of the URI expected with this Intent according to the Common Intents page
      */
     private void openWebPage(String url) {
-        // COMPLETED (2) Use Uri.parse to parse the String into a Uri
         /*
          * We wanted to demonstrate the Uri.parse method because its usage occurs frequently. You
          * could have just as easily passed in a Uri as the parameter of this method.
          */
         Uri webpage = Uri.parse(url);
 
-        // COMPLETED (3) Create an Intent with Intent.ACTION_VIEW and the webpage Uri as parameters
         /*
          * Here, we create the Intent with the action of ACTION_VIEW. This action allows the user
          * to view particular content. In this case, our webpage URL.
          */
         Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
 
-        // COMPLETED (4) Verify that this Intent can be launched and then call startActivity
         /*
          * This is a check we perform with every implicit Intent that we launch. In some cases,
          * the device where this code is running might not have an Activity to perform the action
@@ -112,5 +110,15 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         }
     }
+
+
+    // TODO (1) Create a method called showMap with a Uri as the single parameter
+    // Do steps 2 - 4 within the showMap method
+        // TODO (2) Create an Intent with action type, Intent.ACTION_VIEW
+
+        // TODO (3) Set the data of the Intent to the Uri passed into this method
+
+        // TODO (4) Verify that this Intent can be launched and then call startActivity
+
 
 }
