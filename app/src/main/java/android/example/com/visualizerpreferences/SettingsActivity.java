103cab258d097fc6fd73d61adedd23569d55b5c7
Asser <asser@udacity.com>
2017-04-12 21:38:14 +0200

    T06.02-Solution-MakeAPreferenceFragment

--- app/src/main/java/android/example/com/visualizerpreferences/SettingsActivity.java
+++ app/src/main/java/android/example/com/visualizerpreferences/SettingsActivity.java
@@ -45,18 +45,4 @@ public class SettingsActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-
-    // TODO (2) Create a class called SettingsFragment that extends PreferenceFragmentCompat
-    // TODO (3) In res->xml create a file called pref_visualizer
-    // TODO (4) In pref_visualizer create a preference screen containing a single check box preference
-    // This check box preference should have a default value of true, the key 'show_bass', a
-    // summaryOff of Hidden, a summaryOn of Shown and a title of 'Show Bass'
-
-    // TODO (5) In SettingsFragment's onCreatePreferences method add the preference file using the
-    // addPreferencesFromResource method
-
-
-    // TODO (7) Set the root layout of activity_settings to our newly created SettingsFragment
-    // and remove the padding.
-    // [HINT] Use a <fragment> element in xml
 }
