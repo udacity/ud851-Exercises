1cc813471f0c408042df6bd93f0e807c063959e5
Asser <asser@udacity.com>
2017-04-12 20:58:28 +0200

    T01.02-Exercise-DisplayToyList

--- app/src/main/java/com/example/android/favoritetoys/MainActivity.java
+++ app/src/main/java/com/example/android/favoritetoys/MainActivity.java
@@ -20,9 +20,17 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    // TODO (1) Declare a TextView variable called mToysListTextView
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // TODO (3) Use findViewById to get a reference to the TextView from the layout
+
+        // TODO (4) Use the static ToyBox.getToyNames method and store the names in a String array
+
+        // TODO (5) Loop through each toy and append the name to the TextView (add \n for spacing)
     }
 }
