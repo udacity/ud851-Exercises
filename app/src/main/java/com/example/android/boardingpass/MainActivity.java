142fe7f220fd6a5b66565313cb90b66a130010eb
Asser <asser@udacity.com>
2017-04-12 22:29:43 +0200

    T11.03-Exercise-LandscapeLayout

--- app/src/main/java/com/example/android/boardingpass/MainActivity.java
+++ app/src/main/java/com/example/android/boardingpass/MainActivity.java
@@ -29,7 +29,6 @@ import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
-    //COMPLETED (3) Create a data binding instance called mBinding of type ActivityMainBinding
     ActivityMainBinding mBinding;
 
     @Override
@@ -38,28 +37,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        // COMPLETED (4) Set the Content View using DataBindingUtil to the activity_main layout
         /*
          * DataBindUtil.setContentView replaces our normal call of setContent view.
          * DataBindingUtil also created our ActivityMainBinding that we will eventually use to
          * display all of our data.
          */
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        // COMPLETED (5) Load a BoardingPassInfo object with fake data using FakeDataUtils
         BoardingPassInfo fakeBoardingInfo = FakeDataUtils.generateFakeBoardingPassInfo();
-        // COMPLETED (9) Call displayBoardingPassInfo and pass the fake BoardingInfo instance
         displayBoardingPassInfo(fakeBoardingInfo);
     }
 
     private void displayBoardingPassInfo(BoardingPassInfo info) {
 
-        // COMPLETED (6) Use mBinding to set the Text in all the textViews using the data in info
         mBinding.textViewPassengerName.setText(info.passengerName);
+        // TODO (7) Use the flightInfo attribute in mBinding below to get the appropriate text Views
         mBinding.textViewOriginAirport.setText(info.originCode);
         mBinding.textViewFlightCode.setText(info.flightCode);
         mBinding.textViewDestinationAirport.setText(info.destCode);
 
-        // COMPLETED (7) Use a SimpleDateFormat formatter to set the formatted value in time text views
         SimpleDateFormat formatter = new SimpleDateFormat(getString(R.string.timeFormat), Locale.getDefault());
         String boardingTime = formatter.format(info.boardingTime);
         String departureTime = formatter.format(info.departureTime);
@@ -69,7 +64,6 @@ public class MainActivity extends AppCompatActivity {
         mBinding.textViewDepartureTime.setText(departureTime);
         mBinding.textViewArrivalTime.setText(arrivalTime);
 
-        // COMPLETED (8) Use TimeUnit methods to format the total minutes until boarding
         long totalMinutesUntilBoarding = info.getMinutesUntilBoarding();
         long hoursUntilBoarding = TimeUnit.MINUTES.toHours(totalMinutesUntilBoarding);
         long minutesLessHoursUntilBoarding =
@@ -80,6 +74,7 @@ public class MainActivity extends AppCompatActivity {
                 minutesLessHoursUntilBoarding);
 
         mBinding.textViewBoardingInCountdown.setText(hoursAndMinutesUntilBoarding);
+        // TODO (8) Use the boardingInfo attribute in mBinding below to get the appropriate text Views
         mBinding.textViewTerminal.setText(info.departureTerminal);
         mBinding.textViewGate.setText(info.departureGate);
         mBinding.textViewSeat.setText(info.seatNumber);
