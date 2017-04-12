4085d8d1fa535db3388bef45f1a75c1642aa51a3
Asser <asser@udacity.com>
2017-04-12 21:18:38 +0200

    T04a.01-Solution-AddNewActivity

---
+++ app/src/main/java/com/example/android/explicitintent/ChildActivity.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.explicitintent;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+// COMPLETED (1) Use Android Studio's Activity wizard to create a new Activity called ChildActivity
+public class ChildActivity extends AppCompatActivity {
+
+    // COMPLETED (6) Create a TextView field to display your message
+    /* Field to store our TextView */
+    private TextView mDisplayText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child);
+
+        // COMPLETED (7) Get a reference to your TextView in Java
+        /* Typical usage of findViewById... */
+        mDisplayText = (TextView) findViewById(R.id.tv_display);
+    }
+}
