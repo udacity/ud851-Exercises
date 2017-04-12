90a52f299cd2cf43cdddea2bc6bfcfd66581caa8
Asser <asser@udacity.com>
2017-04-12 22:23:20 +0200

    T10.02-Exercise-CreateNotification

--- app/src/main/java/com/example/android/background/sync/ReminderTasks.java
+++ app/src/main/java/com/example/android/background/sync/ReminderTasks.java
@@ -19,25 +19,17 @@ import android.content.Context;
 
 import com.example.android.background.utilities.PreferenceUtilities;
 
-// COMPLETED (1) Create a class called ReminderTasks
 public class ReminderTasks {
 
-    //  COMPLETED (2) Create a public static constant String called ACTION_INCREMENT_WATER_COUNT
     public static final String ACTION_INCREMENT_WATER_COUNT = "increment-water-count";
 
-    //  COMPLETED (6) Create a public static void method called executeTask
-//  COMPLETED (7) Add a Context called context and String parameter called action to the parameter list
     public static void executeTask(Context context, String action) {
-// COMPLETED (8) If the action equals ACTION_INCREMENT_WATER_COUNT, call this class's incrementWaterCount
         if (ACTION_INCREMENT_WATER_COUNT.equals(action)) {
             incrementWaterCount(context);
         }
     }
 
-    //  COMPLETED (3) Create a private static void method called incrementWaterCount
-//  COMPLETED (4) Add a Context called context to the argument list
     private static void incrementWaterCount(Context context) {
-//      COMPLETED (5) From incrementWaterCount, call the PreferenceUtility method that will ultimately update the water count
         PreferenceUtilities.incrementWaterCount(context);
     }
 }
