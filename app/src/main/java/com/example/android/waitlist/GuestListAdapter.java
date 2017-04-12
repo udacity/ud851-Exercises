0b8c581149f404d61dc708ec3c9da85458719ed1
Asser <asser@udacity.com>
2017-04-12 21:51:17 +0200

    T07.05-Exercise-AddGuests

--- app/src/main/java/com/example/android/waitlist/GuestListAdapter.java
+++ app/src/main/java/com/example/android/waitlist/GuestListAdapter.java
@@ -13,12 +13,10 @@ import com.example.android.waitlist.data.WaitlistContract;
 
 public class GuestListAdapter extends RecyclerView.Adapter<GuestListAdapter.GuestViewHolder> {
 
-    // COMPLETED (1) Replace the mCount with a new Cursor field called mCursor
     // Holds on to the cursor to display the waitlist
     private Cursor mCursor;
     private Context mContext;
 
-    // COMPLETED (2) Modify the constructor to accept a cursor rather than an integer
     /**
      * Constructor using the context and the db cursor
      * @param context the calling context/activity
@@ -26,7 +24,6 @@ public class GuestListAdapter extends RecyclerView.Adapter<GuestListAdapter.Gues
      */
     public GuestListAdapter(Context context, Cursor cursor) {
         this.mContext = context;
-        // COMPLETED (3) Set the local mCursor to be equal to cursor
         this.mCursor = cursor;
     }
 
@@ -40,28 +37,34 @@ public class GuestListAdapter extends RecyclerView.Adapter<GuestListAdapter.Gues
 
     @Override
     public void onBindViewHolder(GuestViewHolder holder, int position) {
-        // COMPLETED (5) Move the cursor to the passed in position, return if moveToPosition returns false
         // Move the mCursor to the position of the item to be displayed
         if (!mCursor.moveToPosition(position))
             return; // bail if returned null
-        // COMPLETED (6) Call getString on the cursor to get the guest's name
+
+        // Update the view holder with the information needed to display
         String name = mCursor.getString(mCursor.getColumnIndex(WaitlistContract.WaitlistEntry.COLUMN_GUEST_NAME));
-        // COMPLETED (7) Call getInt on the cursor to get the party size
         int partySize = mCursor.getInt(mCursor.getColumnIndex(WaitlistContract.WaitlistEntry.COLUMN_PARTY_SIZE));
-        // COMPLETED (8) Set the holder's nameTextView text to the guest's name
+
         // Display the guest name
         holder.nameTextView.setText(name);
-        // COMPLETED (9) Set the holder's partySizeTextView text to the party size
         // Display the party count
         holder.partySizeTextView.setText(String.valueOf(partySize));
     }
 
+
     @Override
     public int getItemCount() {
-        // COMPLETED (4) Update the getItemCount to return the getCount of the cursor
         return mCursor.getCount();
     }
-    
+
+    // TODO (15) Create a new function called swapCursor that takes the new cursor and returns void
+
+    // TODO (16) Inside, check if the current cursor is not null, and close it if so
+
+    // TODO (17) Update the local mCursor to be equal to  newCursor
+
+    // TODO (18) Check if the newCursor is not null, and call this.notifyDataSetChanged() if so
+
     /**
      * Inner class to hold the views needed to display a single item in the recycler-view
      */
