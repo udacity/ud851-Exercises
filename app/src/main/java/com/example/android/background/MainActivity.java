98b5998999d429954dada9ecf03fa7cb9e35acc5
Asser <asser@udacity.com>
2017-04-12 22:23:20 +0200

    T10.02-Exercise-CreateNotification

--- app/src/main/java/com/example/android/background/MainActivity.java
+++ app/src/main/java/com/example/android/background/MainActivity.java
@@ -84,14 +84,12 @@ public class MainActivity extends AppCompatActivity implements
         mToast = Toast.makeText(this, R.string.water_chug_toast, Toast.LENGTH_SHORT);
         mToast.show();
 
-        // COMPLETED (15) Create an explicit intent for WaterReminderIntentService
         Intent incrementWaterCountIntent = new Intent(this, WaterReminderIntentService.class);
-        // COMPLETED (16) Set the action of the intent to ACTION_INCREMENT_WATER_COUNT
         incrementWaterCountIntent.setAction(ReminderTasks.ACTION_INCREMENT_WATER_COUNT);
-        // COMPLETED (17) Call startService and pass the explicit intent you just created
         startService(incrementWaterCountIntent);
-
     }
+
+    // TODO (15) Create a method called testNotification that triggers NotificationUtils' remindUserBecauseCharging
 
     @Override
     protected void onDestroy() {
---
+++ app/src/main/java/com/example/android/background/utilities/NotificationUtils.java
@@ -0,0 +1,53 @@
+package com.example.android.background.utilities;
+
+/**
+ * Utility class for creating hydration notifications
+ */
+public class NotificationUtils {
+
+    // TODO (7) Create a method called remindUserBecauseCharging which takes a Context.
+    // This method will create a notification for charging. It might be helpful
+    // to take a look at this guide to see an example of what the code in this method will look like:
+    // https://developer.android.com/training/notify-user/build-notification.html
+        // TODO (8) Get the NotificationManager using context.getSystemService
+        // TODO (9) Create a notification channel for Android O devices
+        // TODO (10) In the remindUser method use NotificationCompat.Builder to create a notification
+        // that:
+        // - has a color of R.colorPrimary - use ContextCompat.getColor to get a compatible color
+        // - has ic_drink_notification as the small icon
+        // - uses icon returned by the largeIcon helper method as the large icon
+        // - sets the title to the charging_reminder_notification_title String resource
+        // - sets the text to the charging_reminder_notification_body String resource
+        // - sets the style to NotificationCompat.BigTextStyle().bigText(text)
+        // - sets the notification defaults to vibrate
+        // - uses the content intent returned by the contentIntent helper method for the contentIntent
+        // - automatically cancels the notification when the notification is clicked
+        // TODO (11) If the build version is greater than JELLY_BEAN and lower than OREO,
+        // set the notification's priority to PRIORITY_HIGH.
+        // TODO (12) Trigger the notification by calling notify on the NotificationManager.
+        // Pass in a unique ID of your choosing for the notification and notificationBuilder.build()
+
+
+
+    // TODO (1) Create a helper method called contentIntent with a single parameter for a Context. It
+    // should return a PendingIntent. This method will create the pending intent which will trigger when
+    // the notification is pressed. This pending intent should open up the MainActivity.
+        // TODO (2) Create an intent that opens up the MainActivity
+        // TODO (3) Create a PendingIntent using getActivity that:
+            // - Take the context passed in as a parameter
+            // - Takes an unique integer ID for the pending intent (you can create a constant for
+            //   this integer above
+            // - Takes the intent to open the MainActivity you just created; this is what is triggered
+            //   when the notification is triggered
+            // - Has the flag FLAG_UPDATE_CURRENT, so that if the intent is created again, keep the
+            // intent but update the data
+
+
+    // TODO (4) Create a helper method called largeIcon which takes in a Context as a parameter and
+    // returns a Bitmap. This method is necessary to decode a bitmap needed for the notification.
+        // TODO (5) Get a Resources object from the context.
+        // TODO (6) Create and return a bitmap using BitmapFactory.decodeResource, passing in the
+        // resources object and R.drawable.ic_local_drink_black_24px
+
+
+}
