c3492f4ca76636eb93747398bb8da70bc4ebcc07
crazyfoxfrog <crazyfoxfrog@users.noreply.github.com>
2017-06-17 11:55:06 -0400

    Update WaterReminderIntentService.java
    change method name in TODO (13) to executeTask, since there will be no executeTaskForTag in ReminderTasks class

--- Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/sync/WaterReminderIntentService.java
+++ Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/sync/WaterReminderIntentService.java
@@ -20,4 +20,4 @@
 
 //  TODO (11) Override onHandleIntent
 //      TODO (12) Get the action from the Intent that started this Service
-//      TODO (13) Call ReminderTasks.executeTaskForTag and pass in the action to be performed
+//      TODO (13) Call ReminderTasks.executeTask and pass in the action to be performed
