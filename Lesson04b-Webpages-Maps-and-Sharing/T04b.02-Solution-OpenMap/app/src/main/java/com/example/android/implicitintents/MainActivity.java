18f8b666db6ea42f963e83994d9719201cc96ee4
Marcin Sielski <marcin.sielski@gmail.com>
2017-11-25 23:23:21 +0100

    Fix for query with address

--- Lesson04b-Webpages-Maps-and-Sharing/T04b.02-Solution-OpenMap/app/src/main/java/com/example/android/implicitintents/MainActivity.java
+++ Lesson04b-Webpages-Maps-and-Sharing/T04b.02-Solution-OpenMap/app/src/main/java/com/example/android/implicitintents/MainActivity.java
@@ -55,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
         Uri.Builder builder = new Uri.Builder();
         builder.scheme("geo")
                 .path("0,0")
-                .query(addressString);
+                .appendQueryParameter("q",addressString);
         Uri addressUri = builder.build();
 
         // COMPLETED (7) Replace the Toast with a call to showMap, passing in the Uri from the previous step
