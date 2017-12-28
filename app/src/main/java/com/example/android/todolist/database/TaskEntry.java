c220e89d3241be523a540137864e0c66560705b0
Jose Nieto <dnbit@users.noreply.github.com>
2017-12-28 17:36:35 +0000

    Initial Commit

---
+++ app/src/main/java/com/example/android/todolist/database/TaskEntry.java
@@ -0,0 +1,56 @@
+package com.example.android.todolist.database;
+
+import java.util.Date;
+
+public class TaskEntry {
+
+    private int id;
+    private String description;
+    private int priority;
+    private Date updatedAt;
+
+    public TaskEntry(String description, int priority, Date updatedAt) {
+        this.description = description;
+        this.priority = priority;
+        this.updatedAt = updatedAt;
+    }
+
+    public TaskEntry(int id, String description, int priority, Date updatedAt) {
+        this.id = id;
+        this.description = description;
+        this.priority = priority;
+        this.updatedAt = updatedAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
