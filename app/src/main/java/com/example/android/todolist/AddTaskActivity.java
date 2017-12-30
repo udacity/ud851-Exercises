dcb0d14f8ff269bb0cbf8625eb750e2bbac236ba
Jose Nieto <dnbit@users.noreply.github.com>
2017-12-30 22:35:37 +0000

    T09b.02-Exercise-SaveTaskInDatabaseFromAddTaskActivity

--- app/src/main/java/com/example/android/todolist/AddTaskActivity.java
+++ app/src/main/java/com/example/android/todolist/AddTaskActivity.java
@@ -48,11 +48,15 @@ public class AddTaskActivity extends AppCompatActivity {
 
     private int mTaskId = DEFAULT_TASK_ID;
 
+    // TODO (3) Create AppDatabase member variable for the Database
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
 
         initViews();
+
+        // TODO (4) Initialize member variable for the data base
 
         if (savedInstanceState != null && savedInstanceState.containsKey(INSTANCE_TASK_ID)) {
             mTaskId = savedInstanceState.getInt(INSTANCE_TASK_ID, DEFAULT_TASK_ID);
@@ -103,7 +107,13 @@ public class AddTaskActivity extends AppCompatActivity {
      * It retrieves user input and inserts that new task data into the underlying database.
      */
     public void onSaveButtonClicked() {
-        // Not yet implemented
+        // TODO (5) Create a description variable and assign to it the value in the edit text
+        // TODO (6) Create a priority variable and assign the value returned by getPriorityFromViews()
+        // TODO (7) Create a date variable and assign to it the current Date
+
+        // TODO (8) Create taskEntry variable using the variables defined above
+        // TODO (9) Use the taskDao in the AppDatabase variable to insert the taskEntry
+        // TODO (10) call finish() to come back to MainActivity
     }
 
     /**
