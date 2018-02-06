9a308eb8f54ea5e76e51fd805a90ad3b3e5cc254
luis <luisman1989@gmail.com>
2018-02-06 00:14:50 -0500

    completed Application Activity lifeCycle
    When on rotation occur there is a new tag that appears which is on
    restart.

--- Lesson05a-Android-Lifecycle/T05a.03-Exercise-FixLifecycleDisplayBug/app/src/main/java/com/example/android/lifecycle/MainActivity.java
+++ Lesson05a-Android-Lifecycle/T05a.03-Exercise-FixLifecycleDisplayBug/app/src/main/java/com/example/android/lifecycle/MainActivity.java
@@ -6,6 +6,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     /*
@@ -39,7 +41,8 @@ public class MainActivity extends AppCompatActivity {
      */
     private TextView mLifecycleDisplay;
 
-    // TODO (1) Declare and instantiate a static ArrayList of Strings called mLifecycleCallbacks
+    // TODO (1) Declare and instantiate a static ArrayList of Strings called mLifecycleCallbacks CHECKED
+      public static ArrayList<String> mLifeCycleCallbalks = new ArrayList<String>();
 
     /**
      * Called when the activity is first created. This is where you should do all of your normal
@@ -71,9 +74,14 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        // TODO (4) Iterate backwards through mLifecycleCallbacks, appending each String and a newline to mLifecycleDisplay
+        // TODO (4) Iterate backwards through mLifecycleCallbacks, appending each String and a newline to mLifecycleDisplay CHECKED
+         for(int i = mLifeCycleCallbalks.size()-1; i>=0; i--)
+         {
+             mLifecycleDisplay.append(mLifeCycleCallbalks.get(i).toString()+"/n");
+         }
 
-        // TODO (5) Clear mLifecycleCallbacks after iterating through it
+        // TODO (5) Clear mLifecycleCallbacks after iterating through it CHECKED
+        mLifeCycleCallbalks.clear();
 
         logAndAppend(ON_CREATE);
     }
@@ -133,7 +141,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
 
-        // TODO (2) Add the ON_STOP String to the front of mLifecycleCallbacks
+        // TODO (2) Add the ON_STOP String to the front of mLifecycleCallbacks CHECKED
+        mLifeCycleCallbalks.add(ON_STOP);
 
         logAndAppend(ON_STOP);
     }
@@ -160,7 +169,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
 
-        // TODO (3) Add the ON_DESTROY String to the front of mLifecycleCallbacks
+        // TODO (3) Add the ON_DESTROY String to the front of mLifecycleCallbacks CHECKED
+        mLifeCycleCallbalks.add(ON_DESTROY);
 
         logAndAppend(ON_DESTROY);
     }
