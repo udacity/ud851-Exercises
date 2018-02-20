87980886f103ae1c6238408c8f463cca90ade437
luis <luisman1989@gmail.com>
2018-02-19 22:46:00 -0500

    just finish visualize preferences
    nice implementation tryed something new I was not aware of haha!

---
+++ Lesson06-Visualizer-Preferences/T06.01-Exercise-SetupTheActivity/app/src/main/java/android/example/com/visualizerpreferences/SettingsActivity.java
@@ -0,0 +1,18 @@
+package android.example.com.visualizerpreferences;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by luism on 2/17/2018.
+ */
+
+public class SettingsActivity extends AppCompatActivity{
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
--- Lesson06-Visualizer-Preferences/T06.01-Exercise-SetupTheActivity/app/src/main/java/android/example/com/visualizerpreferences/VisualizerActivity.java
+++ Lesson06-Visualizer-Preferences/T06.01-Exercise-SetupTheActivity/app/src/main/java/android/example/com/visualizerpreferences/VisualizerActivity.java
@@ -17,6 +17,8 @@ package android.example.com.visualizerpreferences;
  */
 
 import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.example.com.visualizerpreferences.AudioVisuals.AudioInputReader;
 import android.example.com.visualizerpreferences.AudioVisuals.VisualizerView;
@@ -25,6 +27,9 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 public class VisualizerActivity extends AppCompatActivity {
@@ -44,7 +49,7 @@ public class VisualizerActivity extends AppCompatActivity {
 
     private void defaultSetup() {
         mVisualizerView.setShowBass(true);
-        mVisualizerView.setShowMid(true);
+       // mVisualizerView.setShowMid(true);
         mVisualizerView.setShowTreble(true);
         mVisualizerView.setMinSizeScale(1);
         mVisualizerView.setColor(getString(R.string.pref_color_red_value));
@@ -114,15 +119,43 @@ public class VisualizerActivity extends AppCompatActivity {
 
         }
     }
-
-    // TODO (1) Create a new Empty Activity named SettingsActivity; make sure to generate the
+    // TODO (1) Create a new Empty Activity named SettingsActivity; make sure to generate the CHECKED
     // activity_settings.xml layout file as well and add the activity to the manifest
 
-    // TODO (2) Add a new resource folder called menu and create visualizer_menu.xml
-    // TODO (3) In visualizer_menu.xml create a menu item with a single item. The id should be
+    // TODO (2) Add a new resource folder called menu and create visualizer_menu.xml CHECKED
+    // TODO (3) In visualizer_menu.xml create a menu item with a single item. The id should be CHECKED
     // "action_settings", title should be saved in strings.xml, the item should never
     // be shown as an action, and orderInCategory should be 100
 
-    // TODO (5) Add the menu to the menu bar
-    // TODO (6) When the "Settings" menu item is pressed, open SettingsActivity
+    // TODO (5) Add the menu to the menu bar CHECKED
+    // TODO (6) When the "Settings" menu item is pressed, open SettingsActivity CHECKED
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+         MenuInflater inflater = getMenuInflater();
+         inflater.inflate(R.menu.visualizer_menu,menu);
+         return true;
+    }
+
+
+    //this is the right way to start another intent from current parent activity
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemRes = item.getItemId();
+        switch(itemRes)
+        {
+            case R.id.action_setting:
+                Toast.makeText(getApplicationContext(),"item "+itemRes,Toast.LENGTH_LONG).show();
+
+                Intent SettingActivity = new Intent();
+                Context ourContext = VisualizerActivity.this;
+                Class childActivity =  SettingsActivity.class;
+
+                     SettingActivity.setClass(ourContext,childActivity);
+                     startActivity(SettingActivity);
+
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
