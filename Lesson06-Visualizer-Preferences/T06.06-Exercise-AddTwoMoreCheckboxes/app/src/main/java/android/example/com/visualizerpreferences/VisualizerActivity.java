45a9d6eab34ebdb374a976da0eea7342682c065a
luis <luisman1989@gmail.com>
2018-02-23 14:01:15 -0500

    amazing implementation of sharedPreference
    all gets saved even if the phone is off the app start on its previous
    state ..is some sort of cookie hahahaha wao nice  work am more than
    welcome to be in this classs

--- Lesson06-Visualizer-Preferences/T06.06-Exercise-AddTwoMoreCheckboxes/app/src/main/java/android/example/com/visualizerpreferences/VisualizerActivity.java
+++ Lesson06-Visualizer-Preferences/T06.06-Exercise-AddTwoMoreCheckboxes/app/src/main/java/android/example/com/visualizerpreferences/VisualizerActivity.java
@@ -55,8 +55,11 @@ public class VisualizerActivity extends AppCompatActivity implements SharedPrefe
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         mVisualizerView.setShowBass(sharedPreferences.getBoolean(getString(R.string.pref_show_bass_key),
                 getResources().getBoolean(R.bool.pref_show_bass_default)));
-        mVisualizerView.setShowMid(true);
-        mVisualizerView.setShowTreble(true);
+        mVisualizerView.setShowMid(sharedPreferences.getBoolean(getString(R.string.pref_show_mid_rage_key),
+                getResources().getBoolean(R.bool.pref_show_mid_rage_default)));
+        mVisualizerView.setShowTreble(sharedPreferences.getBoolean(getString(R.string.pref_show_treble_key),
+                getResources().getBoolean(R.bool.pref_show_treble_default)));
+
         mVisualizerView.setMinSizeScale(1);
         mVisualizerView.setColor(getString(R.string.pref_color_red_value));
         // Register the listener
@@ -67,6 +70,14 @@ public class VisualizerActivity extends AppCompatActivity implements SharedPrefe
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
         if (key.equals(getString(R.string.pref_show_bass_key))) {
             mVisualizerView.setShowBass(sharedPreferences.getBoolean(key, getResources().getBoolean(R.bool.pref_show_bass_default)));
+        }
+        if(key.equals(getString(R.string.pref_show_treble_key)))
+        {
+            mVisualizerView.setShowTreble(sharedPreferences.getBoolean(key, getResources().getBoolean(R.bool.pref_show_treble_default)));
+        }
+        if(key.equals(getString(R.string.pref_show_mid_rage_key)))
+        {
+            mVisualizerView.setShowMid(sharedPreferences.getBoolean(key, getResources().getBoolean(R.bool.pref_show_mid_rage_default)));
         }
     }
 
