97e2ac35cf555b5ae995edc2965cdeb82f481578
sackerman <ackerman.steven@gmail.com>
2018-03-17 13:32:00 -0700

    Lesson 9 Updated 3/17

--- Lesson09-ToDo-List/T09.03-Exercise-UriMatcher/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
+++ Lesson09-ToDo-List/T09.03-Exercise-UriMatcher/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
@@ -19,6 +19,7 @@ package com.example.android.todolist.data;
 import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.annotation.NonNull;
@@ -26,13 +27,17 @@ import android.support.annotation.NonNull;
 // Verify that TaskContentProvider extends from ContentProvider and implements required methods
 public class TaskContentProvider extends ContentProvider {
 
-    // TODO (1) Define final integer constants for the directory of tasks and a single item.
+    //Completed: TODO (1) Define final integer constants for the directory of tasks and a single item.
     // It's convention to use 100, 200, 300, etc for directories,
     // and related ints (101, 102, ..) for items in that directory.
+    public static final int TASKS = 100;
+    public static final int TASK_WITH_ID = 101;
+
+
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+
 
     // TODO (3) Declare a static variable for the Uri matcher that you construct
-
-    // TODO (2) Define a static buildUriMatcher method that associates URI's with their int match
 
     // Member variable for a TaskDbHelper that's initialized in the onCreate() method
     private TaskDbHelper mTaskDbHelper;
@@ -50,6 +55,23 @@ public class TaskContentProvider extends ContentProvider {
         Context context = getContext();
         mTaskDbHelper = new TaskDbHelper(context);
         return true;
+    }
+    //Completed: TODO (2) Define a static buildUriMatcher method that associates URI's with their int match
+    //Looked at Solution Code:
+    public static UriMatcher buildUriMatcher() {
+
+        // Initialize a UriMatcher with no matches by passing in NO_MATCH to the constructor
+        UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+        /*
+          All paths added to the UriMatcher have a corresponding int.
+          For each kind of uri you may want to access, add the corresponding match with addURI.
+          The two calls below add matches for the task directory and a single item by ID.
+         */
+        uriMatcher.addURI(TaskContract.AUTHORITY, TaskContract.PATH_TASKS, TASKS);
+        uriMatcher.addURI(TaskContract.AUTHORITY, TaskContract.PATH_TASKS + "/#", TASK_WITH_ID);
+
+        return uriMatcher;
     }
 
 
--- Lesson09-ToDo-List/T09.04-Exercise-Insert/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
+++ Lesson09-ToDo-List/T09.04-Exercise-Insert/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
@@ -17,13 +17,15 @@
 package com.example.android.todolist.data;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.NonNull;
-
+import static com.example.android.todolist.data.TaskContract.TaskEntry.TABLE_NAME;
 // Verify that TaskContentProvider extends from ContentProvider and implements required methods
 public class TaskContentProvider extends ContentProvider {
 
@@ -78,16 +80,34 @@ public class TaskContentProvider extends ContentProvider {
 
     @Override
     public Uri insert(@NonNull Uri uri, ContentValues values) {
-        // TODO (1) Get access to the task database (to write new data to)
+        //Completed: TODO (1) Get access to the task database (to write new data to)
+        final SQLiteDatabase db = mTaskDbHelper.getWritableDatabase();
 
-        // TODO (2) Write URI matching code to identify the match for the tasks directory
+        //Completed: TODO (2) Write URI matching code to identify the match for the tasks directory
+        int match = sUriMatcher.match(uri);
 
-        // TODO (3) Insert new values into the database
-        // TODO (4) Set the value for the returnedUri and write the default case for unknown URI's
+        //Completed: TODO (3) Insert new values into the database
 
-        // TODO (5) Notify the resolver if the uri has been changed, and return the newly inserted URI
+        //Completed: TODO (4) Set the value for the returnedUri and write the default case for unknown URI's
+        Uri returnUri;
+        switch (match) {
+            case TASKS:
+                long id = db.insert(TABLE_NAME, null,values);
+                if (id > 0){
+                    //Yes! It worked!
+                    returnUri = ContentUris.withAppendedId(TaskContract.TaskEntry.CONTENT_URI,id);
+                }else {
+                    throw new android.database.sqlite.SQLiteException("Failed to insert row" + uri);
+        }
+        break;
+        default:
+            throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
 
-        throw new UnsupportedOperationException("Not yet implemented");
+        //Completed: TODO (5) Notify the resolver if the uri has been changed, and return the newly inserted URI
+
+        getContext().getContentResolver().notifyChange(uri, null);
+        return returnUri;
     }
 
 
