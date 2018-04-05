ea9aba144e328399db3b47a8473f4c8f3490a837
luis <luisman1989@gmail.com>
2018-04-05 19:40:31 -0400

    just finished adding the path, authority and base content to my content provider class

--- Lesson09-ToDo-List/T09.02-Exercise-AddURIsToContract/app/src/main/java/com/example/android/todolist/data/TaskContract.java
+++ Lesson09-ToDo-List/T09.02-Exercise-AddURIsToContract/app/src/main/java/com/example/android/todolist/data/TaskContract.java
@@ -16,6 +16,7 @@
 
 package com.example.android.todolist.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 
@@ -30,10 +31,15 @@ public class TaskContract {
         4) Content URI for data in the TaskEntry class
       */
 
+    public static final String AUTHORITY = "com.example.android.todolist";
+    public static final Uri BASE_CONTENT_URI= Uri.parse("content://"+AUTHORITY);
+    public static final String PATH_TASKS  ="tasks";
 
     /* TaskEntry is an inner class that defines the contents of the task table */
     public static final class TaskEntry implements BaseColumns {
 
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon()
+                .appendPath(PATH_TASKS).build();
 
         // Task table and column names
         public static final String TABLE_NAME = "tasks";
