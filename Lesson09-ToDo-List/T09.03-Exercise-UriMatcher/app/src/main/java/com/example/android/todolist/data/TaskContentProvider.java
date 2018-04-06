35e58434acee968689233ea99a14915087f0195d
luis <luisman1989@gmail.com>
2018-04-05 20:55:05 -0400

    well well just finish declaring static variable constant
    just define our Crud functionality content uri

--- Lesson09-ToDo-List/T09.03-Exercise-UriMatcher/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
+++ Lesson09-ToDo-List/T09.03-Exercise-UriMatcher/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
@@ -19,6 +19,7 @@ package com.example.android.todolist.data;
 import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.annotation.NonNull;
@@ -26,13 +27,26 @@ import android.support.annotation.NonNull;
 // Verify that TaskContentProvider extends from ContentProvider and implements required methods
 public class TaskContentProvider extends ContentProvider {
 
-    // TODO (1) Define final integer constants for the directory of tasks and a single item.
+    // TODO (1) Define final integer constants for the directory of tasks and a single item. checked
+    private static final int TASK = 100;
+    private static final int SINGLE_ITEM = 101;
     // It's convention to use 100, 200, 300, etc for directories,
     // and related ints (101, 102, ..) for items in that directory.
 
-    // TODO (3) Declare a static variable for the Uri matcher that you construct
+    // TODO (3) Declare a static variable for the Uri matcher that you construct checked
+     public static UriMatcher uriMatcher =  buildMatcher();
 
-    // TODO (2) Define a static buildUriMatcher method that associates URI's with their int match
+    // TODO (2) Define a static buildUriMatcher method that associates URI's with their int match checked
+    public static UriMatcher buildMatcher()
+    {
+        UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        //DIRECTORY TASK LINK
+        uriMatcher.addURI(TaskContract.AUTHORITY, TaskContract.PATH_TASKS, TASK);
+        //single item task link
+        uriMatcher.addURI(TaskContract.AUTHORITY, TaskContract.PATH_TASKS+"#",SINGLE_ITEM);
+        return uriMatcher;
+
+    }
 
     // Member variable for a TaskDbHelper that's initialized in the onCreate() method
     private TaskDbHelper mTaskDbHelper;
