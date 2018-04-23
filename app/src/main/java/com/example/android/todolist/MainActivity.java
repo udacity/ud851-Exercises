e9faccc60c18d99437810e84f24a398049ec45c9
Jose Nieto <dnbit@users.noreply.github.com>
2018-04-23 22:31:40 +0100

    T09b.03-Solution-RetrieveTasksFromDatabaseAtMainActivity

--- app/src/main/java/com/example/android/todolist/MainActivity.java
+++ app/src/main/java/com/example/android/todolist/MainActivity.java
@@ -26,6 +26,8 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.View;
 
+import com.example.android.todolist.database.AppDatabase;
+
 import static android.support.v7.widget.DividerItemDecoration.VERTICAL;
 
 
@@ -37,7 +39,8 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.ItemC
     private RecyclerView mRecyclerView;
     private TaskAdapter mAdapter;
 
-    // TODO (1) Create AppDatabase member variable for the Database
+    // COMPLETED (1) Create AppDatabase member variable for the Database
+    private AppDatabase mDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -92,7 +95,8 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.ItemC
             }
         });
 
-        // TODO (2) Initialize member variable for the data base
+        // COMPLETED (2) Initialize member variable for the data base
+        mDb = AppDatabase.getInstance(getApplicationContext());
     }
 
     /**
@@ -103,8 +107,9 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.ItemC
     @Override
     protected void onResume() {
         super.onResume();
-        // TODO (3) Call the adapter's setTasks method using the result
+        // COMPLETED (3) Call the adapter's setTasks method using the result
         // of the loadAllTasks method from the taskDao
+        mAdapter.setTasks(mDb.taskDao().loadAllTasks());
     }
 
     @Override
