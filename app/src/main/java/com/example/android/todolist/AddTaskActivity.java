a1de9c23b24162964f1ff6e9eb15f913ca365bc8
Jose Nieto <dnbit@users.noreply.github.com>
2018-04-24 12:28:46 +0100

    T09b.04-Solution-Executors

--- app/src/main/java/com/example/android/todolist/AddTaskActivity.java
+++ app/src/main/java/com/example/android/todolist/AddTaskActivity.java
@@ -115,13 +115,18 @@ public class AddTaskActivity extends AppCompatActivity {
         int priority = getPriorityFromViews();
         Date date = new Date();
 
-        // TODO (4) Make taskEntry final so it is visible inside the run method
-        TaskEntry taskEntry = new TaskEntry(description, priority, date);
-        // TODO (2) Get the diskIO Executor from the instance of AppExecutors and
+        // COMPLETED (4) Make taskEntry final so it is visible inside the run method
+        final TaskEntry taskEntry = new TaskEntry(description, priority, date);
+        // COMPLETED (2) Get the diskIO Executor from the instance of AppExecutors and
         // call the diskIO execute method with a new Runnable and implement its run method
-        // TODO (3) Move the remaining logic inside the run method
-        mDb.taskDao().insertTask(taskEntry);
-        finish();
+        AppExecutors.getInstance().diskIO().execute(new Runnable() {
+            @Override
+            public void run() {
+                // COMPLETED (3) Move the remaining logic inside the run method
+                mDb.taskDao().insertTask(taskEntry);
+                finish();
+            }
+        });
     }
 
     /**
--- app/src/main/java/com/example/android/todolist/MainActivity.java
+++ app/src/main/java/com/example/android/todolist/MainActivity.java
@@ -27,6 +27,9 @@ import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.View;
 
 import com.example.android.todolist.database.AppDatabase;
+import com.example.android.todolist.database.TaskEntry;
+
+import java.util.List;
 
 import static android.support.v7.widget.DividerItemDecoration.VERTICAL;
 
@@ -105,13 +108,25 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.ItemC
     @Override
     protected void onResume() {
         super.onResume();
-        // TODO (5) Get the diskIO Executor from the instance of AppExecutors and
+        // COMPLETED (5) Get the diskIO Executor from the instance of AppExecutors and
         // call the diskIO execute method with a new Runnable and implement its run method
-
-        // TODO (6) Move the logic into the run method and
-        // extract the list of tasks to a final variable
-        // TODO (7) Wrap the setTask call in a call to runOnUiThread
-        mAdapter.setTasks(mDb.taskDao().loadAllTasks());
+        AppExecutors.getInstance().diskIO().execute(new Runnable() {
+            @Override
+            public void run() {
+                // COMPLETED (6) Move the logic into the run method and
+                // Extract the list of tasks to a final variable
+                final List<TaskEntry> tasks = mDb.taskDao().loadAllTasks();
+                // COMPLETED (7) Wrap the setTask call in a call to runOnUiThread
+                // We will be able to simplify this once we learn more
+                // about Android Architecture Components
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mAdapter.setTasks(tasks);
+                    }
+                });
+            }
+        });
     }
 
     @Override
