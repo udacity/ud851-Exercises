02559e0174a358c2f2dde5dc1f733768ee2b1048
Magido Mascate <magidjm@gmail.com>
2018-05-15 17:14:14 +0200

    incorrect namespace

--- Lesson02-GitHub-Repo-Search/T02.01-Exercise-CreateLayout/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ Lesson02-GitHub-Repo-Search/T02.01-Exercise-CreateLayout/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -17,22 +17,25 @@ package com.example.android.datafrominternet;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
 
     // TODO (26) Create an EditText variable called mSearchBoxEditText
-
+	private EditText mSearchBoxEditText;
     // TODO (27) Create a TextView variable called mUrlDisplayTextView
     // TODO (28) Create a TextView variable called mSearchResultsTextView
-
+	private TextView mUrlDisplayTextView, mSearchResultsTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // TODO (29) Use findViewById to get a reference to mSearchBoxEditText
-
+		mSearchBoxEditText = (EditText) findViewById(R.id.edit_query);
         // TODO (30) Use findViewById to get a reference to mUrlDisplayTextView
-        // TODO (31) Use findViewById to get a reference to mSearchResultsTextView
+        mUrlDisplayTextView = (TextView) findViewById(R.id.tv_url_display);
+		// TODO (31) Use findViewById to get a reference to mSearchResultsTextView
+		mSearchResultsTextView = (TextView) findViewById(R.id.tv_github_search_results_jsontv_github_search_results_json);
     }
 }
