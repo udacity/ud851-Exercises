5dd6d206d5955865166523186c5dce35ff8c5416
Lorioux <magidjm@gmail.com>
2018-05-24 08:37:38 +0200

    Solved Exercise T05b.03

--- Lesson05b-Smarter-GitHub-Repo-Search/T05b.03-Exercise-PolishAsyncTask/app/src/main/java/com/example/android/asynctaskloader/MainActivity.java
+++ Lesson05b-Smarter-GitHub-Repo-Search/T05b.03-Exercise-PolishAsyncTask/app/src/main/java/com/example/android/asynctaskloader/MainActivity.java
@@ -166,7 +166,7 @@ public class MainActivity extends AppCompatActivity implements
         return new AsyncTaskLoader<String>(this) {
 
             // TODO (1) Create a String member variable called mGithubJson that will store the raw JSON
-
+            String mGithubJson;
             @Override
             protected void onStartLoading() {
 
@@ -182,7 +182,12 @@ public class MainActivity extends AppCompatActivity implements
                 mLoadingIndicator.setVisibility(View.VISIBLE);
 
                 // TODO (2) If mGithubJson is not null, deliver that result. Otherwise, force a load
-                forceLoad();
+                if (mGithubJson != null){
+                    deliverResult(mGithubJson);
+                }
+                else {
+                    forceLoad();
+                }
             }
 
             @Override
@@ -209,6 +214,11 @@ public class MainActivity extends AppCompatActivity implements
 
             // TODO (3) Override deliverResult and store the data in mGithubJson
             // TODO (4) Call super.deliverResult after storing the data
+            @Override
+            public void deliverResult(String data) {
+                mGithubJson = data;
+                super.deliverResult(data);
+            }
         };
     }
 
