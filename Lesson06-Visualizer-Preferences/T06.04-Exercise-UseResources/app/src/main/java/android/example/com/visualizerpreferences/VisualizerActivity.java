823d8921c9022c7a7850e06b1780c69a05a9b703
Lorioux <magidjm@gmail.com>
2018-05-25 17:08:03 +0200

    COMPLETED Exercise T06.04

--- Lesson06-Visualizer-Preferences/T06.04-Exercise-UseResources/app/src/main/java/android/example/com/visualizerpreferences/VisualizerActivity.java
+++ Lesson06-Visualizer-Preferences/T06.04-Exercise-UseResources/app/src/main/java/android/example/com/visualizerpreferences/VisualizerActivity.java
@@ -51,8 +51,8 @@ public class VisualizerActivity extends AppCompatActivity {
     private void setupSharedPreferences() {
         // Get all of the values from shared preferences to set it up
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        // TODO (4) Use resources here instead of the hard coded string and boolean
-        mVisualizerView.setShowBass(sharedPreferences.getBoolean("show_bass", true));
+        // COMPLETED (4) Use resources here instead of the hard coded string and boolean
+        mVisualizerView.setShowBass(sharedPreferences.getBoolean(getString(R.string.pref_show_bass_key), true));
         mVisualizerView.setShowMid(true);
         mVisualizerView.setShowTreble(true);
         mVisualizerView.setMinSizeScale(1);
