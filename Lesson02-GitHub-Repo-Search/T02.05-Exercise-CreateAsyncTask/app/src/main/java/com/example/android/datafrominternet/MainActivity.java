e2beca732bbcfd9dbba4e7f5c26f43d1fcd25e6d
Apoorva Singh <apoorva.singh2810@gmail.com>
2018-07-01 18:28:37 +0530

    Exercise T02.06 - Completed

--- Lesson02-GitHub-Repo-Search/T02.05-Exercise-CreateAsyncTask/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ Lesson02-GitHub-Repo-Search/T02.05-Exercise-CreateAsyncTask/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -15,6 +15,7 @@
  */
 package com.example.android.datafrominternet;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -68,7 +69,38 @@ public class MainActivity extends AppCompatActivity {
 
     // TODO (1) Create a class called GithubQueryTask that extends AsyncTask<URL, Void, String>
     // TODO (2) Override the doInBackground method to perform the query. Return the results. (Hint: You've already written the code to perform the query)
-    // TODO (3) Override onPostExecute to display the results in the TextView
+
+
+    class GithubQueryTask extends AsyncTask<URL, Void, String>{
+
+        @Override
+        protected String doInBackground(URL... urls) {
+
+            URL searchUrl = urls[0];
+            String githubSearchResults = null;
+
+            try {
+                githubSearchResults = NetworkUtils.getResponseFromHttpUrl(searchUrl);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return githubSearchResults;
+        }
+        // TODO (3) Override onPostExecute to display the results in the TextView
+
+        @Override
+        protected void onPostExecute(String githubSearchResults){
+
+            if(githubSearchResults!=null && !githubSearchResults.equals("")){
+                mSearchResultsTextView.setText(githubSearchResults);
+            }
+        }
+}
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
