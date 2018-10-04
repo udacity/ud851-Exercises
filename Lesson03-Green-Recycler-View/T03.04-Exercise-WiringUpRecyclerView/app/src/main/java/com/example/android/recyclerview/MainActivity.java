8007b33fd3dc46991b4925442d48c2f8ab413f87
Richard Lu <rlu@jimmyjohns.com>
2018-10-03 21:39:57 -0500

    add recyclerview and get reference from layout

--- Lesson03-Green-Recycler-View/T03.04-Exercise-WiringUpRecyclerView/app/src/main/java/com/example/android/recyclerview/MainActivity.java
+++ Lesson03-Green-Recycler-View/T03.04-Exercise-WiringUpRecyclerView/app/src/main/java/com/example/android/recyclerview/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // TODO (4) Use findViewById to store a reference to the RecyclerView in mNumbersList
+        mNumberList = (RecyclerView) findViewById(R.id.rv_numbers);
 
         // TODO (5) Create a LinearLayoutManager variable called layoutManager
         // TODO (6) Use setLayoutManager on mNumbersList with the LinearLayoutManager we created above
