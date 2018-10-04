0349d533caec6faf6838614b268737e6c89d365a
Richard Lu <rlu@jimmyjohns.com>
2018-10-03 22:13:12 -0500

    add interface/protocol called ListItemClickListener

--- Lesson03-Green-Recycler-View/T03.07-Exercise-RecyclerViewClickHandling/app/src/main/java/com/example/android/recyclerview/GreenAdapter.java
+++ Lesson03-Green-Recycler-View/T03.07-Exercise-RecyclerViewClickHandling/app/src/main/java/com/example/android/recyclerview/GreenAdapter.java
@@ -87,8 +87,9 @@ public class GreenAdapter extends RecyclerView.Adapter<GreenAdapter.NumberViewHo
 
     private int mNumberItems;
 
-    // TODO (1) Add an interface called ListItemClickListener
-    // TODO (2) Within that interface, define a void method called onListItemClick that takes an int as a parameter
+    interface ListItemClickListener {
+        void onListItemClick(int position);
+    }
 
     // TODO (4) Add a ListItemClickListener as a parameter to the constructor and store it in mOnClickListener
     /**
