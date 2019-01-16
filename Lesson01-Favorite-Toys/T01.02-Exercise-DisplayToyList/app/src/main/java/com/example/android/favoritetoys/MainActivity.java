b645116aee5046596e9bd606f10b1d92d1a6b19c
Matija <vorkapicmatija@gmail.com>
2019-01-16 12:36:57 +0100

    Add toy TextView and append toy string array to it

--- Lesson01-Favorite-Toys/T01.02-Exercise-DisplayToyList/app/src/main/java/com/example/android/favoritetoys/MainActivity.java
+++ Lesson01-Favorite-Toys/T01.02-Exercise-DisplayToyList/app/src/main/java/com/example/android/favoritetoys/MainActivity.java
@@ -17,20 +17,19 @@ package com.example.android.favoritetoys;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO (1) Declare a TextView variable called mToysListTextView
-
+    TextView mToysListTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // TODO (3) Use findViewById to get a reference to the TextView from the layout
-
-        // TODO (4) Use the static ToyBox.getToyNames method and store the names in a String array
-
-        // TODO (5) Loop through each toy and append the name to the TextView (add \n for spacing)
+        mToysListTextView = (TextView) findViewById(R.id.tv_toy_names);
+        String[] toyNames = ToyBox.getToyNames();
+        for (String toyName : toyNames) {
+            mToysListTextView.append(toyName + "\n\n\n");
+        }
     }
 }
