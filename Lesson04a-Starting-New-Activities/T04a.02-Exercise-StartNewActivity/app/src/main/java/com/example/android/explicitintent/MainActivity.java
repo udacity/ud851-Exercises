ee5e480a7dcb2ed4b106ecabcd5e303267997269
Matija <vorkapicmatija@gmail.com>
2019-02-12 22:49:06 +0100

    Create an intent and start ChildActivity from MainActivity using it

--- Lesson04a-Starting-New-Activities/T04a.02-Exercise-StartNewActivity/app/src/main/java/com/example/android/explicitintent/MainActivity.java
+++ Lesson04a-Starting-New-Activities/T04a.02-Exercise-StartNewActivity/app/src/main/java/com/example/android/explicitintent/MainActivity.java
@@ -16,6 +16,7 @@
 package com.example.android.explicitintent;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -62,13 +63,11 @@ public class MainActivity extends AppCompatActivity {
                 Context context = MainActivity.this;
 
                 // TODO (1) Store ChildActivity.class in a Class object called destinationActivity
-
+                Class destinationActivity = ChildActivity.class;
                 // TODO (2) Create an Intent to start ChildActivity
-
+                Intent intent = new Intent(context, destinationActivity);
                 // TODO (3) Replace the Toast with code to start ChildActivity
-                String message = "Button clicked!\nTODO: Start a new Activity and pass some data.";
-                Toast.makeText(context, message, Toast.LENGTH_LONG).show();
-
+                startActivity(intent);
             }
         });
     }
