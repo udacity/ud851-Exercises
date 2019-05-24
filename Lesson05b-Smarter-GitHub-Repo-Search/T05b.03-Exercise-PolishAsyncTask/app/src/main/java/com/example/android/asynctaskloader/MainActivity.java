352aacf661ffcd8d12addb005b16128b9bfe9e87
Matija <vorkapicmatija@gmail.com>
2019-05-24 21:30:46 +0200

    Cache results when user navigates from the app

--- Lesson05b-Smarter-GitHub-Repo-Search/T05b.03-Exercise-PolishAsyncTask/app/src/main/java/com/example/android/asynctaskloader/MainActivity.java
+++ Lesson05b-Smarter-GitHub-Repo-Search/T05b.03-Exercise-PolishAsyncTask/app/src/main/java/com/example/android/asynctaskloader/MainActivity.java
@@ -167,6 +167,8 @@ public class MainActivity extends AppCompatActivity implements
 
             // TODO (1) Create a String member variable called mGithubJson that will store the raw JSON
 
+            String mGithubJson;
+
             @Override
             protected void onStartLoading() {
 
@@ -181,9 +183,13 @@ public class MainActivity extends AppCompatActivity implements
                  * When we initially begin loading in the background, we want to display the
                  * loading indicator to the user
                  */
-                mLoadingIndicator.setVisibility(View.VISIBLE);
+                if (mGithubJson != null) {
+                    deliverResult(mGithubJson);
+                } else {
+                    mLoadingIndicator.setVisibility(View.VISIBLE);
 
-                forceLoad();
+                    forceLoad();
+                }
             }
 
             @Override
@@ -210,6 +216,12 @@ public class MainActivity extends AppCompatActivity implements
 
             // TODO (3) Override deliverResult and store the data in mGithubJson
             // TODO (4) Call super.deliverResult after storing the data
+
+            @Override
+            public void deliverResult(String data) {
+                mGithubJson = data;
+                super.deliverResult(data);
+            }
         };
     }
 
