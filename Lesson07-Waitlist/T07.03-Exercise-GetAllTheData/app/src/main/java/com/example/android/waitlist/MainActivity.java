0a249d04ab339da718f9f8559c66c07071ebc21a
Matija <vorkapicmatija@gmail.com>
2019-06-13 16:28:29 +0200

    Get all guest data and pass the row count to the GuestListAdapter

--- Lesson07-Waitlist/T07.03-Exercise-GetAllTheData/app/src/main/java/com/example/android/waitlist/MainActivity.java
+++ Lesson07-Waitlist/T07.03-Exercise-GetAllTheData/app/src/main/java/com/example/android/waitlist/MainActivity.java
@@ -1,17 +1,25 @@
 package com.example.android.waitlist;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
+import com.example.android.waitlist.data.TestUtil;
+import com.example.android.waitlist.data.WaitlistContract;
+import com.example.android.waitlist.data.WaitlistDbHelper;
+
+import com.example.android.waitlist.data.WaitlistContract.*;
 
 public class MainActivity extends AppCompatActivity {
 
     private GuestListAdapter mAdapter;
 
     // TODO (1) Create a local field member of type SQLiteDatabase called mDb
+    private SQLiteDatabase mDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +35,13 @@ public class MainActivity extends AppCompatActivity {
         waitlistRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         // Create an adapter for that cursor to display the data
-        mAdapter = new GuestListAdapter(this);
+
+
+        WaitlistDbHelper waitlistDbHelper = new WaitlistDbHelper(this);
+
+        mDb = waitlistDbHelper.getWritableDatabase();
+
+        TestUtil.insertFakeData(mDb);
 
         // TODO (2) Create a WaitlistDbHelper instance, pass "this" to the constructor as context
 
@@ -36,6 +50,10 @@ public class MainActivity extends AppCompatActivity {
         // TODO (4) call insertFakeData from TestUtil and pass the database reference mDb
 
         // TODO (7) Run the getAllGuests function and store the result in a Cursor variable
+
+        Cursor cursor = getAllGuests();
+
+        mAdapter = new GuestListAdapter(this, cursor.getCount());
 
         // TODO (12) Pass the resulting cursor count to the adapter
 
@@ -56,5 +74,15 @@ public class MainActivity extends AppCompatActivity {
     // TODO (5) Create a private method called getAllGuests that returns a cursor
 
     // TODO (6) Inside, call query on mDb passing in the table name and projection String [] order by COLUMN_TIMESTAMP
+
+    private Cursor getAllGuests() {
+        return mDb.query(WaitlistEntry.TABLE_NAME,
+                null,
+                null,
+                null,
+                null,
+                null,
+                WaitlistEntry.COLUMN_TIMESTAMP);
+    }
 
 }
