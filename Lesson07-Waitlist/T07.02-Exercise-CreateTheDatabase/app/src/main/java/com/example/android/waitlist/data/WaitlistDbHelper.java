6cb5576790841bbd014406ab03a8767ab936ddc5
Matija <vorkapicmatija@gmail.com>
2019-06-13 16:06:56 +0200

    Add waitlist database helper class

--- Lesson07-Waitlist/T07.02-Exercise-CreateTheDatabase/app/src/main/java/com/example/android/waitlist/data/WaitlistDbHelper.java
+++ Lesson07-Waitlist/T07.02-Exercise-CreateTheDatabase/app/src/main/java/com/example/android/waitlist/data/WaitlistDbHelper.java
@@ -1,7 +1,38 @@
 package com.example.android.waitlist.data;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.android.waitlist.data.WaitlistContract.*;
+
 // TODO (1) extend the SQLiteOpenHelper class
-public class WaitlistDbHelper {
+public class WaitlistDbHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "waitlist.db";
+    public static final int DATABASE_VERSION = 1;
+
+    public WaitlistDbHelper (Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String SQL_CREATE_WAITLIST_TABLE = "CREATE TABLE " + WaitlistEntry.TABLE_NAME + " (" +
+                                        WaitlistEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                                        WaitlistEntry.COLUMN_GUEST_NAME + " TEXT NOT NULL," +
+                                        WaitlistEntry.COLUMN_PARTY_SIZE + " INTEGER NOT NULL," +
+                                        WaitlistEntry.COLUMN_TIMESTAMP + " TIMESTAMP DEFAULT CURRENT_TIMESTAMP" +
+                                        ");";
+        sqLiteDatabase.execSQL(SQL_CREATE_WAITLIST_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        String SQL_DROP_WAITLIST_TABLE = "DROP TABLE IF EXISTS " + WaitlistEntry.TABLE_NAME;
+        sqLiteDatabase.execSQL(SQL_DROP_WAITLIST_TABLE);
+        onCreate(sqLiteDatabase);
+    }
 
     // TODO (2) Create a static final String called DATABASE_NAME and set it to "waitlist.db"
 
