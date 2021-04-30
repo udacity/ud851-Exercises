8484573444be98aafdea5d4b19a6ce0cf453e616
omkar01 <omkargujja01@gmail.com>
2021-04-30 17:40:22 +0530

    1st exercise completed

--- app/src/main/java/com/example/android/favoritetoys/MainActivity.java
+++ app/src/main/java/com/example/android/favoritetoys/MainActivity.java
@@ -16,7 +16,8 @@
 package com.example.android.favoritetoys;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
